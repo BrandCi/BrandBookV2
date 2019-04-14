8d240ba8320b24df2a156f9e0d61b9ff207119f7
Philipp C. Moser <info@philipp-moser.de>
2019-04-14 17:58:47 +0200

    Created UserService

--- BrandBook.Services/Users/UserService.cs
+++ BrandBook.Services/Users/UserService.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using BrandBook.Core.Domain.User;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Services.Users
 {
-    class UserService
+    public class UserService : UserManager<User>
     {
+        public UserService(IUserStore<User> store)
+            : base(store)
+        {
+        }
+
+        public static UserService Create(IdentityFactoryOptions<UserService> options, IOwinContext context)
+        {
+            var manager = new UserService(new UserStore<User>(context.Get<BrandBookDbContext>()));
+
+            // Validation for usernames
+            manager.UserValidator = new UserValidator<User>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Validation for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            // User lockout defaults
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<User>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
     }
+
 }
