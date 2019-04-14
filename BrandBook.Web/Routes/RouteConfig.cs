2101d29fa2211d6a9b2bce20f25d68fa4764c751
Philipp C. Moser <info@philipp-moser.de>
2019-04-14 18:09:58 +0200

    Moved the routing Config to a own Routes folder

--- BrandBook.Web/Routes/RouteConfig.cs
+++ BrandBook.Web/Routes/RouteConfig.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace BrandBook.Web
+namespace BrandBook.Web.Routes
 {
     public class RouteConfig
     {
