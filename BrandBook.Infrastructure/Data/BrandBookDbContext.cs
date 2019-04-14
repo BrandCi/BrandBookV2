a4b42eacda18ddff8b4eaafba3f6af03df50d65f
Philipp C. Moser <info@philipp-moser.de>
2019-04-14 16:30:28 +0200

    Added IdentityModels to BrandBookDbContext

--- BrandBook.Infrastructure/Data/BrandBookDbContext.cs
+++ BrandBook.Infrastructure/Data/BrandBookDbContext.cs
@@ -4,10 +4,12 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.User;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace BrandBook.Infrastructure.Data
 {
-    public class BrandBookDbContext : DbContext
+    public class BrandBookDbContext : IdentityDbContext<User>
     {
         public BrandBookDbContext()
             : base("name=DefaultConnection")
