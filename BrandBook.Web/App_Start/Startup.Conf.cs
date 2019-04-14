101dd54bd77c33fb281987c6012928d33029632d
Philipp C. Moser <info@philipp-moser.de>
2019-04-14 21:28:45 +0200

    Added Startup.Conf Class

---
+++ BrandBook.Web/App_Start/Startup.Conf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace BrandBook.Web
+{
+	public partial class Startup
+	{
+	}
+}
