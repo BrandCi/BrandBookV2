54c31dab04afe6b416cb9bf77d376d40720a68e1
Philipp C. Moser <info@philipp-moser.de>
2019-04-22 21:37:52 +0200

    Added new View Model - LoginViewModel

---
+++ BrandBook.Web/ViewModels/Authorization/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BrandBook.Web.ViewModels.Authorization
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "E-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Passwort")]
+        public string Password { get; set; }
+    }
+}
