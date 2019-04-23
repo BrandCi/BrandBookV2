aaa2969ef7d4c9563573823249300b927c71ad61
Philipp C. Moser <info@philipp-moser.de>
2019-04-23 15:23:52 +0200

    Added new ImprintViewModel

---
+++ BrandBook.Web/ViewModels/Legal/ImprintViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BrandBook.Core.Domain.Frontend.Legal;
+
+namespace BrandBook.Web.ViewModels.Legal
+{
+    public class ImprintViewModel
+    {
+        public List<ImprintValue> ImprintValues { get; set; }
+    }
+}
