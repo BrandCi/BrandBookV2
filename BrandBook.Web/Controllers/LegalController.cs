334d495b7e646a5ba47bde363f9944a6a0ff96c2
Philipp C. Moser <info@philipp-moser.de>
2019-04-23 14:02:49 +0200

    Added new LegalController for all LegalPages

---
+++ BrandBook.Web/Controllers/LegalController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrandBook.Web.Controllers
+{
+    public class LegalController : Controller
+    {
+        // GET: Legal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Imprint()
+        {
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
