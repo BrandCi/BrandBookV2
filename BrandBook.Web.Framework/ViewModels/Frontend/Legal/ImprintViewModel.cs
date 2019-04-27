cfbf3cffc0f30ee5d4e68476367e472063326e99
Philipp C. Moser <info@philipp-moser.de>
2019-04-27 18:18:04 +0200

    Added Value to ImprintViewModel

--- BrandBook.Web.Framework/ViewModels/Frontend/Legal/ImprintViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/Frontend/Legal/ImprintViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.Frontend.Legal;
 
 namespace BrandBook.Web.Framework.ViewModels.Frontend.Legal
 {
     public class ImprintViewModel
     {
+        public IEnumerable<ImprintValue> ImprintValues { get; set; }
     }
 }
