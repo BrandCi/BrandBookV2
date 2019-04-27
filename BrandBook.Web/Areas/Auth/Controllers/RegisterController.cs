1f8f75330817acb7764134ada30434d8dfbe9652
Philipp C. Moser <info@philipp-moser.de>
2019-04-27 17:44:43 +0200

    Created Registration Process in Area Auth

---
+++ BrandBook.Web/Areas/Auth/Controllers/RegisterController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using BrandBook.Core.Domain.User;
+using BrandBook.Services.Authentication;
+using BrandBook.Services.Users;
+using BrandBook.Web.ViewModels.Authorization;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace BrandBook.Web.Areas.Auth.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        #region Fields
+
+        private SignInService _signInService;
+        private UserService _userService;
+
+        #endregion
+
+        #region Constructor
+
+        public RegisterController() { }
+
+        public RegisterController(UserService userService, SignInService signInService)
+        {
+            UserManager = userService;
+            SignInService = signInService;
+        }
+
+        #endregion
+
+
+
+        public SignInService SignInService
+        {
+            get
+            {
+                return _signInService ?? HttpContext.GetOwinContext().Get<SignInService>();
+            }
+            private set
+            {
+                _signInService = value;
+            }
+        }
+
+        public UserService UserManager
+        {
+            get
+            {
+                return _userService ?? HttpContext.GetOwinContext().GetUserManager<UserService>();
+            }
+            private set
+            {
+                _userService = value;
+            }
+        }
+
+
+        // GET: Auth/Register
+        public ActionResult Index()
+        {
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Index(RegisterViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await UserManager.AddToRoleAsync(user.Id, "User");
+                    await SignInService.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    return RedirectToAction("Index", "Home", new {});
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
