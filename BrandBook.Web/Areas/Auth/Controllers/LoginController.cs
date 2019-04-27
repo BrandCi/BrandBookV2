85cd2b04ab083d84a8311e0a8884e25e215c44fe
Philipp C. Moser <info@philipp-moser.de>
2019-04-27 17:30:55 +0200

    Set up Login Controller and View and matching Layout Pge

---
+++ BrandBook.Web/Areas/Auth/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrandBook.Services.Authentication;
+using BrandBook.Services.Users;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace BrandBook.Web.Areas.Auth.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        #region Fields
+
+        private SignInService _signInService;
+        private UserService _userService;
+
+        #endregion
+
+        #region Constructor
+
+        public LoginController() { }
+
+        public LoginController(UserService userService, SignInService signInService)
+        {
+            UserManager = userService;
+            SignInService = signInService;
+        }
+
+        #endregion
+
+
+
+        public SignInService SignInService
+        {
+            get
+            {
+                return _signInService ?? HttpContext.GetOwinContext().Get<SignInService>();
+            }
+            private set
+            {
+                _signInService = value;
+            }
+        }
+
+        public UserService UserManager
+        {
+            get
+            {
+                return _userService ?? HttpContext.GetOwinContext().GetUserManager<UserService>();
+            }
+            private set
+            {
+                _userService = value;
+            }
+        }
+
+
+
+
+        // GET: Auth/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
