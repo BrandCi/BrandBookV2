731ef71014599227c6b89d4d3c14bf270dceedb2
Philipp C. Moser <info@philipp-moser.de>
2019-04-27 18:15:46 +0200

    Copied ViewModels

--- BrandBook.Web.Framework/ViewModels/Auth/LoginViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/Auth/LoginViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BrandBook.Web.Framework.ViewModels.Auth
 {
     public class LoginViewModel
     {
+
+        [Required]
+        [Display(Name = "Nutzername")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Passwort")]
+        public string Password { get; set; }
+
+        [Display(Name = "Daten speichern")]
+        public bool RememberMe { get; set; }
+
     }
 }
