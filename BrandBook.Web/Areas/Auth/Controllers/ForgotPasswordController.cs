ae1b983adb93a46a0e8cb0da3a0ff5ffecef9bf9
Philipp C. Moser <info@philipp-moser.de>
2019-04-29 12:27:01 +0200

    Added Translations for ForgotPassword View and updated the FP-Controller

--- BrandBook.Web/Areas/Auth/Controllers/ForgotPasswordController.cs
+++ BrandBook.Web/Areas/Auth/Controllers/ForgotPasswordController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Areas.Auth.Controllers
 {
-    public class ForgotPasswordController : Controller
+    public class ForgotPasswordController : AuthControllerBase
     {
         // GET: Auth/ForgotPassword
         public ActionResult Index()
