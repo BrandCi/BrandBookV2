7e61f2eb54005072d7de802bf20b192842303c5d
Philipp C. Moser <info@philipp-moser.de>
2019-04-29 12:42:52 +0200

    Created new Controller for handling CultureChanges

---
+++ BrandBook.Web/Controllers/CultureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrandBook.Web.Controllers
+{
+    public class CultureController : Controller
+    {
+        // GET: Culture
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
