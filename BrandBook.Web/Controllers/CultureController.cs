faa2d5d2af25bb4dab10866323944c1c1d2069cc
Philipp C. Moser <info@philipp-moser.de>
2019-04-29 12:44:01 +0200

    Created Action for Setting the Culture

--- BrandBook.Web/Controllers/CultureController.cs
+++ BrandBook.Web/Controllers/CultureController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Controllers
 {
-    public class CultureController : Controller
+    public class CultureController : FrontendControllerBase
     {
-        // GET: Culture
-        public ActionResult Index()
+
+        public ActionResult SetCulture()
         {
-            return View();
+            return RedirectToAction("Index", "Home", new{area = ""});
         }
     }
 }
