7968293f607730feab2f8af07fe7345ee8cc0394
Philipp C. Moser <info@philipp-moser.de>
2019-04-30 21:28:08 +0200

    Impelmented UserRepository in the UserRepository class

--- BrandBook.Infrastructure/Repositories/User/UserRepository.cs
+++ BrandBook.Infrastructure/Repositories/User/UserRepository.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.RepositoryInterfaces.User;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories.User
 {
-    class UserRepository
+    internal class UserRepository : Repository<Core.Domain.User.User>, IUserRepository
     {
+        internal UserRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
+
+
+        public Core.Domain.User.User FindByUsername(string username)
+        {
+            return Set.FirstOrDefault(x => x.UserName == username);
+        }
+
     }
 }
