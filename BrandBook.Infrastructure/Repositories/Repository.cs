d3c390ea5ea8f7a980911317b9c66b0b97cf516c
Philipp C. Moser <info@philipp-moser.de>
2019-04-30 21:21:10 +0200

    Implemented all currently required methods from IRepository in Repository.
    Created needed Methods

--- BrandBook.Infrastructure/Repositories/Repository.cs
+++ BrandBook.Infrastructure/Repositories/Repository.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.RepositoryInterfaces;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories
 {
-    internal class Repository
+    internal class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
+        private BrandBookDbContext _context;
+        private DbSet<TEntity> _set;
+
+        internal Repository(BrandBookDbContext context)
+        {
+            _context = context;
+        }
+
+        protected DbSet<TEntity> Set
+        {
+            get
+            {
+                return _set ?? (_set = _context.Set<TEntity>());
+            }
+
+        }
+
+
+        public List<TEntity> GetAll()
+        {
+            return Set.ToList();
+        }
+
     }
 }
