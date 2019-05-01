690e84f7afa5962c936f1f7171851c80dd6fd143
Philipp C. Moser <info@philipp-moser.de>
2019-05-01 13:32:58 +0200

    Added Regions to IUnitOfWork
    #22

--- BrandBook.Core/IUnitOfWork.cs
+++ BrandBook.Core/IUnitOfWork.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.RepositoryInterfaces.User;
 
 namespace BrandBook.Core
 {
     public interface IUnitOfWork : IDisposable
     {
+
+        #region Fileds
+        IAppUserRepository UserRepository { get; }
+        IUserRoleRepository UserRoleRepository { get; }
+        IRolePermissionRepository RolePermissionRepository { get; }
+        #endregion
+
+
     }
 }
