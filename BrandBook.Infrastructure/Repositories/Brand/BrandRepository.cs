b2ab0e58abfb02940f04219a28ffd2c667f1cb71
Philipp C. Moser <info@philipp-moser.de>
2019-05-01 16:14:12 +0200

    Finalized BrandRepository Implementation

--- BrandBook.Infrastructure/Repositories/Brand/BrandRepository.cs
+++ BrandBook.Infrastructure/Repositories/Brand/BrandRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories.Brand
 {
-    class BrandRepository
+    internal class BrandRepository : Repository<Core.Domain.Brand.Brand>
     {
+        internal BrandRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
     }
 }
