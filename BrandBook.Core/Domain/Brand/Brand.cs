ce9c4b958930b8614a2240b63daddca10a55dde4
Philipp C. Moser <info@philipp-moser.de>
2019-05-01 16:08:42 +0200

    Added Properties to Brand DomainModel

--- BrandBook.Core/Domain/Brand/Brand.cs
+++ BrandBook.Core/Domain/Brand/Brand.cs
@@ -8,6 +8,9 @@ namespace BrandBook.Core.Domain.Brand
 {
     public class Brand : BaseEntity
     {
-
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string MainColor { get; set; }
     }
 }
