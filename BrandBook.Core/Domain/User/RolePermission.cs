59fd4734cada5d9ff9c2d2a0ee42e08970d9f28f
Philipp C. Moser <info@philipp-moser.de>
2019-05-01 12:19:00 +0200

    Created Properties for RolePermission Class
    #22

--- BrandBook.Core/Domain/User/RolePermission.cs
+++ BrandBook.Core/Domain/User/RolePermission.cs
@@ -8,5 +8,11 @@ namespace BrandBook.Core.Domain.User
 {
     public class RolePermission
     {
+
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
     }
 }
