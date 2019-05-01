3a385f4914ca276ebe0dfd4cd2729d767a2daec5
Philipp C. Moser <info@philipp-moser.de>
2019-05-01 16:22:12 +0200

    Impelent IEnumerable for BrandOverviewViewModel

--- BrandBook.Web.Framework/ViewModels/App/Brand/BrandOverviewViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/BrandOverviewViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,18 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Brand
 {
-    public class BrandOverviewViewModel
+    public class BrandOverviewViewModel : IEnumerable<SingleBrandOverviewViewModel>
     {
         public List<SingleBrandOverviewViewModel> Brands { get; set; }
+
+        public IEnumerator<SingleBrandOverviewViewModel> GetEnumerator()
+        {
+            return Brands.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
     }
 }
