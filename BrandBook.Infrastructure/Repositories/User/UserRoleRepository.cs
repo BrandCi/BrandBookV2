37b2f93883dc8732c2fa402c9b26ae4572125820
Philipp C. Moser <info@philipp-moser.de>
2019-05-01 13:23:36 +0200

    Provide DbContext to BaseClass

--- BrandBook.Infrastructure/Repositories/User/UserRoleRepository.cs
+++ BrandBook.Infrastructure/Repositories/User/UserRoleRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.User;
+using BrandBook.Core.RepositoryInterfaces.User;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories.User
 {
-    class UserRoleRepository
+    internal class UserRoleRepository : Repository<UserRole>, IUserRoleRepository
     {
+        internal UserRoleRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
     }
 }
