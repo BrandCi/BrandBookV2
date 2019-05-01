e0d724357c84f00b44969decc6e661638389bf8d
Philipp C. Moser <info@philipp-moser.de>
2019-05-01 13:24:59 +0200

    Finalized the RolePermissionRepository

--- BrandBook.Infrastructure/Repositories/User/RolePermissionRepository.cs
+++ BrandBook.Infrastructure/Repositories/User/RolePermissionRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.User;
+using BrandBook.Core.RepositoryInterfaces.User;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories.User
 {
-    class RolePermissionRepository
+    internal class RolePermissionRepository : Repository<RolePermission>, IRolePermissionRepository
     {
+        internal RolePermissionRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
     }
 }
