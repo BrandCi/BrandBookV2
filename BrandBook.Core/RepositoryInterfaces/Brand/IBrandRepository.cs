66c0e5017737d44e6bd4ea91abaf1ba3437cfe69
Philipp C. Moser <info@philipp-moser.de>
2019-05-01 16:11:18 +0200

    Added IBrandRepository

---
+++ BrandBook.Core/RepositoryInterfaces/Brand/IBrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandBook.Core.RepositoryInterfaces.Brand
+{
+    interface IBrandRepository
+    {
+    }
+}
