684fa0148f590d93e983fc7cbafcf35157e51f7b
Philipp C. Moser <info@philipp-moser.de>
2019-05-05 16:20:46 +0200

    Add Usage of Repository in BrandController Api

--- BrandBook.Web/Api/BrandController.cs
+++ BrandBook.Web/Api/BrandController.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
+using BrandBook.Core.RepositoryInterfaces.Brand;
+using BrandBook.Infrastructure.Data;
+using BrandBook.Infrastructure.Repositories.Brand;
 
 namespace BrandBook.Web.Api
 {
     public class BrandController : ApiController
     {
+
+        private IBrandRepository brandRepository;
+
+        public BrandController()
+        {
+            this.brandRepository = new BrandRepository(new BrandBookDbContext());
+        }
+
+
     }
 }
