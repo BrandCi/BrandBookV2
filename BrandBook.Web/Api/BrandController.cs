17b10c4ac98f5e23c3d284a0f8d54d869b66f9ef
Philipp C. Moser <info@philipp-moser.de>
2019-05-05 13:13:01 +0200

    Created first WebApi Controller

---
+++ BrandBook.Web/Api/BrandController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BrandBook.Web.Api
+{
+    public class BrandController : ApiController
+    {
+    }
+}
