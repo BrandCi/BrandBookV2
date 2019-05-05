4ad8584ccbf15fdff5fb9e35a96df668cde57224
Philipp C. Moser <info@philipp-moser.de>
2019-05-05 13:30:29 +0200

    Update MappingProfile.cs

--- BrandBook.Infrastructure/Mapper/MappingProfile.cs
+++ BrandBook.Infrastructure/Mapper/MappingProfile.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace BrandBook.Infrastructure.Mapper
 {
-    class MappingProfile
+    public class MappingProfile : Profile
     {
     }
 }
