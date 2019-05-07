1fd305ea9e997f5d1393ad30bafafb80e28a1c9b
Philipp C. Moser <info@philipp-moser.de>
2019-05-07 22:39:20 +0200

    Create new SettingsController

---
+++ BrandBook.Web/Areas/App/Controllers/SettingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrandBook.Web.Areas.App.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: App/Settings
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
