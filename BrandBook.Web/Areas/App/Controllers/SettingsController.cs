fcabd0f8ae9dabe38966b9c09f20127c027a2058
Philipp C. Moser <info@philipp-moser.de>
2019-05-07 23:23:31 +0200

    Restrict Access for SettingsController to authroized persons

--- BrandBook.Web/Areas/App/Controllers/SettingsController.cs
+++ BrandBook.Web/Areas/App/Controllers/SettingsController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Areas.App.Controllers
 {
-    public class SettingsController : Controller
+    public class SettingsController : AppControllerBase
     {
         // GET: App/Settings
         public ActionResult Index()
