40d2134d9593753824a09835d68e90d98e3ebac8
Philipp C. Moser <info@philipp-moser.de>
2019-05-08 21:23:38 +0200

    Created new Action for BRandSettings

--- BrandBook.Web/Areas/App/Controllers/SettingsController.cs
+++ BrandBook.Web/Areas/App/Controllers/SettingsController.cs
@@ -31,5 +31,10 @@ namespace BrandBook.Web.Areas.App.Controllers
         {
             return View();
         }
+
+        public ActionResult Brand()
+        {
+            return View();
+        }
     }
 }
