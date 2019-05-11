c53fdf6b40db77c888ee08f5c332a749d9ccdd73
Philipp C. Moser <info@philipp-moser.de>
2019-05-11 11:27:40 +0200

    Add new BrandControlelr

---
+++ BrandBook.Web/Areas/App/Controllers/BrandController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrandBook.Web.Areas.App.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: App/Brand
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
