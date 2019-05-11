a1df72859c370f77bd39d1a5c664132c22b2b169
Philipp C. Moser <info@philipp-moser.de>
2019-05-11 12:39:27 +0200

    Update SettingViewModel.cs

--- BrandBook.Web.Framework/ViewModels/App/Settings/SettingViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Settings/SettingViewModel.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Settings
 {
-    class SettingViewModel
+    public class SettingViewModel
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Key { get; set; }
+
+        public string Value { get; set; }
     }
 }
