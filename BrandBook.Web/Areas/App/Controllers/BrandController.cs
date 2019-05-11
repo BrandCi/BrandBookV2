7ff2272ffe04073fa1cfe5f4af482c66d161520b
Philipp C. Moser <info@philipp-moser.de>
2019-05-11 11:27:57 +0200

    Inherit from AppControllerBase

--- BrandBook.Web/Areas/App/Controllers/BrandController.cs
+++ BrandBook.Web/Areas/App/Controllers/BrandController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Areas.App.Controllers
 {
-    public class BrandController : Controller
+    public class BrandController : AppControllerBase
     {
         // GET: App/Brand
         public ActionResult Index()
