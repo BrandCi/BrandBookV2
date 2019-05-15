634555b78d2541c110ebd33989788625c76637fb
Philipp C. Moser <info@philipp-moser.de>
2019-05-15 22:56:54 +0200

    UserController must inherit from AppControllerBase

--- BrandBook.Web/Areas/App/Controllers/UserController.cs
+++ BrandBook.Web/Areas/App/Controllers/UserController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Areas.App.Controllers
 {
-    public class UserController : Controller
+    public class UserController : AppControllerBase
     {
         // GET: App/User
         public ActionResult UserOverview()
