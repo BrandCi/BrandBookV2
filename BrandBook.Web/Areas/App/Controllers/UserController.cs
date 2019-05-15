33a9e975e3b318961d1c909992304060fdf9112a
Philipp C. Moser <info@philipp-moser.de>
2019-05-15 19:26:31 +0200

    Create new UserController

---
+++ BrandBook.Web/Areas/App/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrandBook.Web.Areas.App.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: App/User
+        public ActionResult UserOverview()
+        {
+            return View();
+        }
+    }
+}
