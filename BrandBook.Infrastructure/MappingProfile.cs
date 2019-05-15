d1d1239d9989c37631d5d5e5cff83212e59248e4
Philipp C. Moser <info@philipp-moser.de>
2019-05-15 21:11:38 +0200

    Add MappingProfiles for AppUserDto <-> AppUser

--- BrandBook.Infrastructure/MappingProfile.cs
+++ BrandBook.Infrastructure/MappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using BrandBook.Core.Domain.Brand;
+using BrandBook.Core.Domain.User;
 using BrandBook.Core.Dtos.Brand;
+using BrandBook.Core.Dtos.User;
 
 namespace BrandBook.Infrastructure
 {
@@ -10,6 +12,9 @@ namespace BrandBook.Infrastructure
         {
             CreateMap<Brand, BrandDto>();
             CreateMap<BrandDto, Brand>();
+
+            CreateMap<AppUserDto, AppUser>();
+            CreateMap<AppUser, AppUserDto>();
         }
     }
 }
