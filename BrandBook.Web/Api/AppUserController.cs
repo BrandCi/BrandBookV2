dd9a3c73d16e260963bbdf80f71bfa4fce3e9586
Philipp C. Moser <info@philipp-moser.de>
2019-05-15 21:02:13 +0200

    Add new ApiController for AppUser

---
+++ BrandBook.Web/Api/AppUserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BrandBook.Core.RepositoryInterfaces.User;
+using BrandBook.Infrastructure.Data;
+using BrandBook.Infrastructure.Repositories.User;
+
+namespace BrandBook.Web.Api
+{
+    public class AppUserController : ApiController
+    {
+
+        private IAppUserRepository appUserRepository;
+
+        public AppUserController()
+        {
+            this.appUserRepository = new AppUserRepository(new BrandBookDbContext());
+        }
+
+    }
+}
