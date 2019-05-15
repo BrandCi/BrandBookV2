d5a5fe9f32e436542296224228a5f644b5d0df3c
Philipp C. Moser <info@philipp-moser.de>
2019-05-15 21:17:54 +0200

    Get all current users

--- BrandBook.Web/Api/AppUserController.cs
+++ BrandBook.Web/Api/AppUserController.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
+using BrandBook.Core.Domain.Brand;
+using BrandBook.Core.Domain.User;
+using BrandBook.Core.Dtos.Brand;
+using BrandBook.Core.Dtos.User;
 using BrandBook.Core.RepositoryInterfaces.User;
 using BrandBook.Infrastructure.Data;
 using BrandBook.Infrastructure.Repositories.User;
 
 namespace BrandBook.Web.Api
 {
+    [Authorize]
     public class AppUserController : ApiController
     {
 
@@ -18,6 +24,15 @@ namespace BrandBook.Web.Api
         public AppUserController()
         {
             this.appUserRepository = new AppUserRepository(new BrandBookDbContext());
+        }
+
+
+
+        public IHttpActionResult GetAppUsers()
+        {
+            var allAppUser = appUserRepository.GetAll().Select(Mapper.Map<AppUser, AppUserDto>);
+
+            return Ok(allAppUser);
         }
 
     }
