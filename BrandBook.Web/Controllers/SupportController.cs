11f4a2dadee2fda3a250ba7305f69a124ce1c974
Philipp C. Moser <info@philipp-moser.de>
2019-05-19 18:08:35 +0200

    SupportController should inherit from FrontendControllerBase

--- BrandBook.Web/Controllers/SupportController.cs
+++ BrandBook.Web/Controllers/SupportController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Controllers
 {
-    public class SupportController : Controller
+    public class SupportController : FrontendControllerBase
     {
         public ActionResult Contact()
         {
