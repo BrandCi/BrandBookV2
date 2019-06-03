bf25e189f3772e130683abd6cb377d1d8e294c5f
Philipp C. Moser <info@philipp-moser.de>
2019-06-03 22:41:11 +0200

    Create Method SendEmailAsynch()

--- BrandBook.Services/Email/EmailService.cs
+++ BrandBook.Services/Email/EmailService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,11 @@ namespace BrandBook.Services.Email
 {
     public class EmailService
     {
+
+        public async Task<bool> SendEmailAsync()
+        {
+            return false;
+        }
+
     }
 }
