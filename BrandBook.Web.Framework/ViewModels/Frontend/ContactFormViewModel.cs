1ed33fbd415f468cdf6e4297ea3e0ece61463cd1
Philipp C. Moser <info@philipp-moser.de>
2019-06-03 21:48:38 +0200

    Add DataAnnotations to ContactFormViewModel

--- BrandBook.Web.Framework/ViewModels/Frontend/ContactFormViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/Frontend/ContactFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,18 @@ namespace BrandBook.Web.Framework.ViewModels.Frontend
 {
     public class ContactFormViewModel
     {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
         public string Subject { get; set; }
+
+        [Required]
         public string Message { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Please accept Privacy Policy")]
         public bool AcceptPrivacyPolicy { get; set; }
     }
 }
