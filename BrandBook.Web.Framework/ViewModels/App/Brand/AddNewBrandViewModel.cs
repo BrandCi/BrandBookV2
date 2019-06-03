d5dfdb11640ddcc2c54a3bcec3adc073911b8aad
Philipp C. Moser <info@philipp-moser.de>
2019-06-03 20:47:21 +0200

    Set up AddNewBrandViewModel with Properties

--- BrandBook.Web.Framework/ViewModels/App/Brand/AddNewBrandViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/AddNewBrandViewModel.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Brand
 {
-    class AddNewBrandViewModel
+    public class AddNewBrandViewModel
     {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string MainColor { get; set; }
+
+        public string Image { get; set; }
+
+        public bool AgreeTerms { get; set; }
+
+
+
     }
 }
