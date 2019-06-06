2b887a96b23e73984a144347f3cfa0fa727aa635
Philipp C. Moser <info@philipp-moser.de>
2019-06-06 13:59:27 +0200

    Add Properties to GeneralSettingsViewModel

--- BrandBook.Web.Framework/ViewModels/App/Brand/Settings/GeneralSettingsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Settings/GeneralSettingsViewModel.cs
@@ -8,5 +8,8 @@ namespace BrandBook.Web.Framework.ViewModels.App.Brand.Settings
 {
     public class GeneralSettingsViewModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MainHexColor { get; set; }
     }
 }
