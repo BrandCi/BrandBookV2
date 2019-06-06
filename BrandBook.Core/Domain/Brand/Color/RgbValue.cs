755fb5e229bbbe6ae2f5274b433fb4a5e8e19f20
Philipp C. Moser <info@philipp-moser.de>
2019-06-06 23:42:25 +0200

    Add Properties for RgbValue

--- BrandBook.Core/Domain/Brand/Color/RgbValue.cs
+++ BrandBook.Core/Domain/Brand/Color/RgbValue.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Core.Domain.Brand.Color
 {
-    class RgbValue
+    public class RgbValue
     {
+
+        public int Id { get; set; }
+
+
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+
     }
 }
