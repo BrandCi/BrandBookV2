6467cf654d438ddc35b99655e6821d830645f6e9
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 00:05:03 +0200

    Add Constructor for ColorRepository

--- BrandBook.Infrastructure/Repositories/Brand/ColorRepository.cs
+++ BrandBook.Infrastructure/Repositories/Brand/ColorRepository.cs
@@ -5,10 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using BrandBook.Core.Domain.Brand.Color;
 using BrandBook.Core.RepositoryInterfaces.Brand;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories.Brand
 {
     public class ColorRepository : Repository<Color>, IColorRepository
     {
+
+        public ColorRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
+
     }
 }
