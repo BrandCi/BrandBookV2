f6f287416609f0e23dd5c14002ce2cae41addc75
Philipp C. Moser <info@philipp-moser.de>
2019-06-06 22:11:46 +0200

    Enter Data Annotations and ErrorMessages for ViewModels
    ContactSettingsViewModel
    GeneralSettingsViewModel

--- BrandBook.Web.Framework/ViewModels/App/Brand/Settings/ContactSettingsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Settings/ContactSettingsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace BrandBook.Web.Framework.ViewModels.App.Brand.Settings
     public class ContactSettingsViewModel
     {
         public int Id { get; set; }
+
+        [Required (ErrorMessage = "Please define your Contact Person Email Address")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Please enter a valid Email Address")]
         public string ContactPerson { get; set; }
     }
 }
--- BrandBook.Web.Framework/ViewModels/App/Brand/Settings/GeneralSettingsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Settings/GeneralSettingsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,14 @@ namespace BrandBook.Web.Framework.ViewModels.App.Brand.Settings
 {
     public class GeneralSettingsViewModel
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required (ErrorMessage = "Please define a Name for your Brand")]
         public string Name { get; set; }
+
+        [StringLength(6, MinimumLength = 6,
+            ErrorMessage = "Your Hex-Value should be exactly 6 chars")]
         public string MainHexColor { get; set; }
     }
 }
