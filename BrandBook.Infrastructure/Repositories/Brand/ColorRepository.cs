c813e784856f73341087fb01e908f17f0f86a197
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 00:04:36 +0200

    Prepare ColorRepository

--- BrandBook.Infrastructure/Repositories/Brand/ColorRepository.cs
+++ BrandBook.Infrastructure/Repositories/Brand/ColorRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.Brand.Color;
+using BrandBook.Core.RepositoryInterfaces.Brand;
 
 namespace BrandBook.Infrastructure.Repositories.Brand
 {
-    class ColorRepository
+    public class ColorRepository : Repository<Color>, IColorRepository
     {
     }
 }
