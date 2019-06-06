13ab4d8cb0ccdd588a535bfa51474b4c6af8f048
Philipp C. Moser <info@philipp-moser.de>
2019-06-06 13:58:02 +0200

    Summarize General and Contact -SettingsViewModel in BrandSettingsViewModel

--- BrandBook.Web.Framework/ViewModels/App/Brand/Settings/BrandSettingsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Settings/BrandSettingsViewModel.cs
@@ -8,5 +8,7 @@ namespace BrandBook.Web.Framework.ViewModels.App.Brand.Settings
 {
     public class BrandSettingsViewModel
     {
+        public GeneralSettingsViewModel GeneralSettingsViewModel { get; set; }
+        public ContactSettingsViewModel ContactSettingsViewModel { get; set; }
     }
 }
