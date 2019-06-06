e887515c81963ed9c1386f2cb29e2ac6e22b728c
Philipp C. Moser <info@philipp-moser.de>
2019-06-06 13:07:25 +0200

    Add "ShortDescription" Property to Brand Domain Model

--- BrandBook.Core/Domain/Brand/Brand.cs
+++ BrandBook.Core/Domain/Brand/Brand.cs
@@ -11,6 +11,7 @@ namespace BrandBook.Core.Domain.Brand
         public string Name { get; set; }
         public string ImageName { get; set; }
         public string ImageType { get; set; }
+        public string ShortDescription { get; set; }
         public string Description { get; set; }
         public string MainHexColor { get; set; }
     }
