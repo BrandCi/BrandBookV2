54d3a8f6f3136ca3bfc55b3fde42fa6f80860a50
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 10:36:47 +0200

    Add CustomizingSettingsVm to BrandSettingsViewModel as Parameter

--- BrandBook.Web.Framework/ViewModels/App/Brand/Settings/BrandSettingsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Settings/BrandSettingsViewModel.cs
@@ -10,5 +10,6 @@ namespace BrandBook.Web.Framework.ViewModels.App.Brand.Settings
     {
         public GeneralSettingsViewModel GeneralSettingsViewModel { get; set; }
         public ContactSettingsViewModel ContactSettingsViewModel { get; set; }
+        public CustomizingSettingsViewModel CustomizingSettingsViewModel { get; set; }
     }
 }
