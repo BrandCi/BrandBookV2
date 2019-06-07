31e80896be7aa78f2d99c82a251205394039c5f0
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 10:47:34 +0200

    Adding Properties to CustomizingSettingsViewModel

--- BrandBook.Web.Framework/ViewModels/App/Brand/Settings/CustomizingSettingsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Settings/CustomizingSettingsViewModel.cs
@@ -9,6 +9,8 @@ namespace BrandBook.Web.Framework.ViewModels.App.Brand.Settings
 {
     public class CustomizingSettingsViewModel
     {
-
+        public string PrimaryHexColor { get; set; }
+        public bool RoundedButtons { get; set; }
+        public int RoundedButtonsPixel { get; set; }
     }
 }
