bcf1eba028d449eaabf362a0a1dea646cbf0b6a9
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 14:59:35 +0200

    Inherit from PublicControllerBase

--- BrandBook.Web/Areas/Public/Controllers/FontsController.cs
+++ BrandBook.Web/Areas/Public/Controllers/FontsController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Areas.Public.Controllers
 {
-    public class FontsController : Controller
+    public class FontsController : PublicControllerBase
     {
         // GET: Public/Fonts
         public ActionResult Index()
