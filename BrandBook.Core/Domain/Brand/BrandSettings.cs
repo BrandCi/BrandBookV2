fdf96993a4fd5d2a3435a2ad2c3a071161c840e0
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 11:10:22 +0200

    Add all relevant BrandSetting Properties

--- BrandBook.Core/Domain/Brand/BrandSettings.cs
+++ BrandBook.Core/Domain/Brand/BrandSettings.cs
@@ -8,5 +8,9 @@ namespace BrandBook.Core.Domain.Brand
 {
     public class BrandSettings : BaseEntity
     {
+        public string ContactEmail { get; set; }
+        public string PrimaryHexColor { get; set; }
+        public bool RoundedButtons { get; set; }
+        public int RoundedButtonsPixel { get; set; }
     }
 }
