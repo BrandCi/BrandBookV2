98206ee1cc98134b079eaab539d1a99f86a4f66c
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 15:01:58 +0200

    Add new ColorsController

---
+++ BrandBook.Web/Areas/Public/Controllers/ColorsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
+
+namespace BrandBook.Web.Areas.Public.Controllers
+{
+    public class ColorsController : PublicControllerBase
+    {
+        // GET: Public/Colors
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
