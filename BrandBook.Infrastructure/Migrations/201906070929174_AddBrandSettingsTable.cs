e9d2a51cf6a25de4d00a25ba9fe5878a05fb13e5
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 11:30:33 +0200

    Add Migration Queries to Migration

--- BrandBook.Infrastructure/Migrations/201906070929174_AddBrandSettingsTable.cs
+++ BrandBook.Infrastructure/Migrations/201906070929174_AddBrandSettingsTable.cs
@@ -22,12 +22,20 @@ namespace BrandBook.Infrastructure.Migrations
             AddColumn("dbo.Brands", "BrandSetting_Id", c => c.Int());
             CreateIndex("dbo.Brands", "BrandSetting_Id");
             AddForeignKey("dbo.Brands", "BrandSetting_Id", "dbo.BrandSettings", "Id");
+
+            // Migrate current available Data to BrandSettings
+            Sql("INSERT INTO dbo.BrandSettings (ContactEmail) SELECT ContactPerson FROM dbo.Brands");
+
             DropColumn("dbo.Brands", "ContactPerson");
         }
         
         public override void Down()
         {
             AddColumn("dbo.Brands", "ContactPerson", c => c.String());
+
+            // Migrate current available Data to Brands
+            Sql("INSERT INTO dbo.Brands (ContactPerson) SELECT ContactEmail FROM dbo.BrandSettings");
+
             DropForeignKey("dbo.Brands", "BrandSetting_Id", "dbo.BrandSettings");
             DropIndex("dbo.Brands", new[] { "BrandSetting_Id" });
             DropColumn("dbo.Brands", "BrandSetting_Id");
