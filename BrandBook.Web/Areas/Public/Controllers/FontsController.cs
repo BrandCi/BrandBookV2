6bd6ed756bb3766f800533197ac780a497ba1757
Philipp C. Moser <info@philipp-moser.de>
2019-06-07 14:59:12 +0200

    Create new Public Fonts Overview

---
+++ BrandBook.Web/Areas/Public/Controllers/FontsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrandBook.Web.Areas.Public.Controllers
+{
+    public class FontsController : Controller
+    {
+        // GET: Public/Fonts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
