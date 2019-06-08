5587ad9dc361001b81d8663a77dc3b3edb6938ee
Philipp C. Moser <info@philipp-moser.de>
2019-06-08 23:32:40 +0200

    Add Properties to FontInclusion Domain Model

--- BrandBook.Core/Domain/Brand/Font/FontInclusion.cs
+++ BrandBook.Core/Domain/Brand/Font/FontInclusion.cs
@@ -8,5 +8,8 @@ namespace BrandBook.Core.Domain.Brand.Font
 {
     public class FontInclusion : BaseEntity
     {
+        public string InlineIntegration { get; set; }
+        public string CssImport { get; set; }
+        public string CssProperty { get; set; }
     }
 }
