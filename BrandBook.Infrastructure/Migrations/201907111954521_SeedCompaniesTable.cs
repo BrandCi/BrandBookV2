625ffec3daa2d0ee6ff558b35890ed8b89103259
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-11 22:01:52 +0200

    Add empty migration "201907111954521_SeedCompaniesTable"

---
+++ BrandBook.Infrastructure/Migrations/201907111954521_SeedCompaniesTable.cs
@@ -0,0 +1,16 @@
+namespace BrandBook.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedCompaniesTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
