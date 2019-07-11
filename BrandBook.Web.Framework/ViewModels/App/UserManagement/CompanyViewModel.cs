f413484a7587cbbb04ebf21c20c742a87fd5c1b0
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-11 22:50:52 +0200

    Add CompaniesOverviewViewModel

--- BrandBook.Web.Framework/ViewModels/App/UserManagement/CompanyViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/UserManagement/CompanyViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.UserManagement
 {
-    public class CompanyViewModel
+
+    public class CompaniesOverviewViewModel : IEnumerable<SingleCompanyViewModel>
+    {
+        public List<SingleCompanyViewModel> Companies { get; set; }
+
+        public IEnumerator<SingleCompanyViewModel> GetEnumerator()
+        {
+            return Companies.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class SingleCompanyViewModel
     {
 
         public string Name { get; set; }
@@ -14,4 +31,5 @@ namespace BrandBook.Web.Framework.ViewModels.App.UserManagement
         public string ContactEmail { get; set; }
 
     }
+
 }
