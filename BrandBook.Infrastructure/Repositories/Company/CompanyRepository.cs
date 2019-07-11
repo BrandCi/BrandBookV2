1019be70ee5c3ea4c9a73424c7f9d203ba70d785
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-11 22:30:18 +0200

    Update CompanyRepository.cs

--- BrandBook.Infrastructure/Repositories/Company/CompanyRepository.cs
+++ BrandBook.Infrastructure/Repositories/Company/CompanyRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BrandBook.Core.Repositories.Company;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories.Company
 {
-    class CompanyRepository
+    public class CompanyRepository : Repository<Core.Domain.Company.Company>, ICompanyRepository
     {
+
+        public CompanyRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
+
+
     }
 }
