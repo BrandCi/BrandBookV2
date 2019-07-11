071a7ec9a0076766f9f7af5815516829db0c208d
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-11 21:51:30 +0200

    Add Company to AppUserdomain Model

--- BrandBook.Core/Domain/User/AppUser.cs
+++ BrandBook.Core/Domain/User/AppUser.cs
@@ -15,6 +15,8 @@ namespace BrandBook.Core.Domain.User
         public string LastName { get; set; }
         public bool PrivacyPolicyAccepted { get; set; }
 
+        public Company.Company Company { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<AppUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
