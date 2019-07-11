f4d40aeedddbfa6612e5d6cdfad5ce7fd31b9d83
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-11 22:28:30 +0200

    Update ICompanyRepository.cs

--- BrandBook.Core/Repositories/Company/ICompanyRepository.cs
+++ BrandBook.Core/Repositories/Company/ICompanyRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace BrandBook.Core.Repositories.Company
 {
-    interface ICompanyRepository
+    public interface ICompanyRepository : IRepository<Domain.Company.Company>
     {
     }
 }
