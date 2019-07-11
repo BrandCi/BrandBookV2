0ab321c69e94733fb4b9b0f6bce0cc7acc988092
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-11 22:18:57 +0200

    Update SeedMethod

--- BrandBook.Infrastructure/Migrations/201907111954521_SeedCompaniesTable.cs
+++ BrandBook.Infrastructure/Migrations/201907111954521_SeedCompaniesTable.cs
@@ -7,10 +7,24 @@ namespace BrandBook.Infrastructure.Migrations
     {
         public override void Up()
         {
+
+            Sql("SET IDENTITY_INSERT [dbo].[Companies] ON");
+            Sql("INSERT INTO[dbo].[Companies]([Id], [Name], [UrlName], [ContactEmail]) VALUES(1, N'Philipp C. Moser', N'philipp-c-moser', N'info@philipp-moser.de')");
+            Sql("SET IDENTITY_INSERT[dbo].[Companies] OFF");
+
+
+
+            Sql("UPDATE [dbo].[AspNetUsers] SET Company_Id = 1 WHERE  Id = N'2d370030-c2c5-4196-a9e2-c423398630e3'");
+
+
+
         }
         
         public override void Down()
         {
+
+            Sql("DELETE FROM [dbo].[Companies] WHERE Id = 1");
+
         }
     }
 }
