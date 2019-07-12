ffd28460b1c1ea368553d2ddf4793887d0178ac1
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-12 19:09:46 +0200

    Update IBrandRepository.cs

--- BrandBook.Core/Repositories/Brand/IBrandRepository.cs
+++ BrandBook.Core/Repositories/Brand/IBrandRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace BrandBook.Core.Repositories.Brand
+﻿using System.Collections.Generic;
+
+namespace BrandBook.Core.Repositories.Brand
 {
     public interface IBrandRepository : IRepository<Domain.Brand.Brand>
     {
+
+        List<Domain.Brand.Brand> GetBrandsByCompany(int companyId);
+
     }
 }
