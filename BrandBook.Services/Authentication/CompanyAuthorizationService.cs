73417fdfcf48652b27aa7fc76432799aae978f9a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-12 19:01:17 +0200

    Create "GetBrandAsync" Method from CompanyAuthorizationService

--- BrandBook.Services/Authentication/CompanyAuthorizationService.cs
+++ BrandBook.Services/Authentication/CompanyAuthorizationService.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core;
+using BrandBook.Core.Domain.Brand;
+using BrandBook.Core.Domain.Company;
+using BrandBook.Infrastructure;
 
 namespace BrandBook.Services.Authentication
 {
-    class CompanyAuthorizationService
+    public class CompanyAuthorizationService
     {
+
+        private IUnitOfWork _unitOfWork;
+
+        public CompanyAuthorizationService()
+        {
+            this._unitOfWork = new UnitOfWork();
+        }
+
+
+        public Task<Brand> GetBrandAsync(int AppUserId, int BrandId)
+        {
+
+            
+
+
+        }
+
+
     }
 }
