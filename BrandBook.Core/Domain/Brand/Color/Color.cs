07e26b9ddf6317afa0a66f2474c6c2dc383b2fd9
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-13 17:40:53 +0200

    Add Relationship Ids ForeignKeys to Color Domain Model

--- BrandBook.Core/Domain/Brand/Color/Color.cs
+++ BrandBook.Core/Domain/Brand/Color/Color.cs
@@ -9,9 +9,14 @@ namespace BrandBook.Core.Domain.Brand.Color
     public class Color : BaseEntity
     {
         public string Name { get; set; }
+
+        public int CategoryId { get; set; }
         public ColorCategory Category { get; set; }
 
+        public int RgbValueId { get; set; }
         public RgbValue RgbValue { get; set; }
+
+        public int CmykValueId { get; set; }
         public CmykValue CmykValue { get; set; }
 
     }
