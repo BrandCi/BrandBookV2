d3b3eb531b6664ddbe0f7f0aad66e37b0ee2de2b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-13 09:48:28 +0200

    Set up UserSettingsViewModel

--- BrandBook.Web.Framework/ViewModels/App/Settings/UserSettingsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Settings/UserSettingsViewModel.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Settings
 {
-    class UserSettingsViewModel
+    public class UserSettingsViewModel
     {
+        public string Password_ReqLength { get; set; }
+        public string Password_ReqNonLetterOrDigit { get; set; }
+        public string Password_ReqDigit { get; set; }
+        public string Password_ReqLowerCase { get; set; }
+        public string Password_ReqUpperCase { get; set; }
+
+
+        public string Lockout_Enabled { get; set; }
+        public string Lockout_LockoutTime { get; set; }
+        public string Lockout_FailedAttempts { get; set; }
     }
 }
