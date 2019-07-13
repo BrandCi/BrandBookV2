2357cb0c61e8f4cae5af01b85139179fb0994b16
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-13 21:25:14 +0200

    Implement IRepository in IColorCategoryRepository

--- BrandBook.Core/Repositories/Brand/IColorCategoryRepository.cs
+++ BrandBook.Core/Repositories/Brand/IColorCategoryRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.Brand.Color;
 
 namespace BrandBook.Core.Repositories.Brand
 {
-    interface IColorCategoryRepository
+    public interface IColorCategoryRepository : IRepository<ColorCategory>
     {
     }
 }
