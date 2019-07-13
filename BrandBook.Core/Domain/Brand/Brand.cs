ec7aafb1d9b614618ec6277f708426015e30c27b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-13 12:46:57 +0200

    Add IsPublic Property to Brand Domain Model

--- BrandBook.Core/Domain/Brand/Brand.cs
+++ BrandBook.Core/Domain/Brand/Brand.cs
@@ -15,6 +15,9 @@ namespace BrandBook.Core.Domain.Brand
         public string Description { get; set; }
         public string MainHexColor { get; set; }
 
+        public bool IsPublic { get; set; }
+
+        public int BrandSettingId { get; set; }
         public BrandSetting BrandSetting { get; set; }
 
         public int CompanyId { get; set; }
