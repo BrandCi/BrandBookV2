baefc842156c626edf8a2fb3a2bf766edbe661c8
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-13 17:57:32 +0200

    Add Migration "201907131557070_AddHexColorCodeToColorsTable"

---
+++ BrandBook.Infrastructure/Migrations/201907131557070_AddHexColorCodeToColorsTable.cs
@@ -0,0 +1,18 @@
+namespace BrandBook.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddHexColorCodeToColorsTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Colors", "HexColorCode", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Colors", "HexColorCode");
+        }
+    }
+}
