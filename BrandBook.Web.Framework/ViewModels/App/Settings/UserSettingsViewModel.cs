c88953fd2069b829691b1569e50137c78500fb4a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-13 10:04:27 +0200

    Add Property names to UserSettingsViewModel

--- BrandBook.Web.Framework/ViewModels/App/Settings/UserSettingsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Settings/UserSettingsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,29 @@ namespace BrandBook.Web.Framework.ViewModels.App.Settings
 {
     public class UserSettingsViewModel
     {
+        [Display(Name="Required Length")]
         public string Password_ReqLength { get; set; }
+
+        [Display(Name = "Require Non Letter or Digit")]
         public string Password_ReqNonLetterOrDigit { get; set; }
+
+        [Display(Name = "Reequire Digit")]
         public string Password_ReqDigit { get; set; }
+
+        [Display(Name = "Require Lowercase")]
         public string Password_ReqLowerCase { get; set; }
+
+        [Display(Name = "Require Uppercase")]
         public string Password_ReqUpperCase { get; set; }
 
 
+        [Display(Name = "Enabled")]
         public string Lockout_Enabled { get; set; }
+
+        [Display(Name = "Lockout Time")]
         public string Lockout_LockoutTime { get; set; }
+
+        [Display(Name = "Max Failed Attempts")]
         public string Lockout_FailedAttempts { get; set; }
     }
 }
