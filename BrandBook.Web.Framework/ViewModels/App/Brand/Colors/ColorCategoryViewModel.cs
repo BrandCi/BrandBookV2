9007182839139ab3af3f6e6735561deceee70160
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-13 21:54:15 +0200

    Add Properties to ColorCategoryViewModel

--- BrandBook.Web.Framework/ViewModels/App/Brand/Colors/ColorCategoryViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Colors/ColorCategoryViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Brand.Colors
 {
-    class ColorCategoryViewModel
+    public class ColorCategoryViewModel : IEnumerable<SingleColorViewModel>
     {
+
+        public string Name { get; set; }
+        public List<SingleColorViewModel> Colors { get; set; }
+
+        public IEnumerator<SingleColorViewModel> GetEnumerator()
+        {
+            return Colors.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
     }
 }
