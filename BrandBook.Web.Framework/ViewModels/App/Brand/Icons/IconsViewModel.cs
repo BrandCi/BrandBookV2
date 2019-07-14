fe862da9528b8165345ca211dd47791be2159abd
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-14 11:22:44 +0200

    Add List of IconCategoryViewModels to IconsViewModel and implement IEnumerable

--- BrandBook.Web.Framework/ViewModels/App/Brand/Icons/IconsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Icons/IconsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Brand.Icons
 {
-    public class IconsViewModel
+    public class IconsViewModel : IEnumerable<IconCategoryViewModel>
     {
+
+        public List<IconCategoryViewModel> Categories { get; set; }
+
+        public IEnumerator<IconCategoryViewModel> GetEnumerator()
+        {
+            return Categories.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
     }
 }
