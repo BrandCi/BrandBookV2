e15f593c8688f28d71927657ea25e25d0cf9d3d8
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-14 16:46:55 +0200

    Get SettingData and pass it via GoogleAnalyticsViewModel to PartialView

--- BrandBook.Web/Controllers/LayoutController.cs
+++ BrandBook.Web/Controllers/LayoutController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using BrandBook.Core;
 using BrandBook.Infrastructure;
 using BrandBook.Web.Framework.Controllers;
+using BrandBook.Web.Framework.ViewModels.Frontend.Layout;
 
 namespace BrandBook.Web.Controllers
 {
@@ -22,8 +23,28 @@ namespace BrandBook.Web.Controllers
         public PartialViewResult GoogleAnalytics()
         {
             var ga_enabled = _unitOfWork.SettingRepository.GetSettingByKey("google_analytics_enabled");
+            GoogleAnalyticsViewModel model;
 
-            return PartialView("GoogleAnalytics");
+            if (ga_enabled.Value == "1")
+            {
+                model = new GoogleAnalyticsViewModel()
+                {
+                    IsActive = true,
+                    TrackingKey = _unitOfWork.SettingRepository.GetSettingByKey("google_analytics_trackingkey").Value
+                };
+            }
+            else
+            {
+                model = new GoogleAnalyticsViewModel()
+                {
+                    IsActive = false,
+                    TrackingKey = ""
+                };
+            }
+
+            
+
+            return PartialView("GoogleAnalytics", model);
         }
 
     }
