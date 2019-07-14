2eb1c8759dedd70d525d4818a8f126419adb4b36
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-14 11:13:16 +0200

    Add IconCategoryRepository and implement IIconCategoryRepository

---
+++ BrandBook.Infrastructure/Repositories/Brand/IconCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrandBook.Core.Domain.Brand.Icon;
+using BrandBook.Core.Repositories.Brand;
+using BrandBook.Infrastructure.Data;
+
+namespace BrandBook.Infrastructure.Repositories.Brand
+{
+    public class IconCategoryRepository : Repository<IconCategory>, IIconCategoryRepository
+    {
+        public IconCategoryRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
