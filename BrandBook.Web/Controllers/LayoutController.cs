d2eeb76519e211c3c02084e8948e639791a945e1
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-14 16:40:01 +0200

    Inject Repositorys during UnitOfWork in LayoutController

--- BrandBook.Web/Controllers/LayoutController.cs
+++ BrandBook.Web/Controllers/LayoutController.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Core;
+using BrandBook.Infrastructure;
 using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Controllers
 {
     public class LayoutController : FrontendControllerBase
     {
+
+        private IUnitOfWork _unitOfWork;
+
+        public LayoutController()
+        {
+            this._unitOfWork = new UnitOfWork();
+        }
 
         public PartialViewResult GoogleAnalytics()
         {
