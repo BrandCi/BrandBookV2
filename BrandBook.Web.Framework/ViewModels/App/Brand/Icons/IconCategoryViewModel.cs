4a0b2909b5327fdb1e171e5800ab3ba7543b4e6b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-14 11:21:50 +0200

    Add new IconCategoryViewModel

---
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Icons/IconCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandBook.Web.Framework.ViewModels.App.Brand.Icons
+{
+    public class IconCategoryViewModel
+    {
+        public string Name { get; set; }
+    }
+}
