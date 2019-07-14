aaa5f1fee75f4b7b8aa146a451909be12270a1ac
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-14 13:24:38 +0200

    Add RoleViewModel

---
+++ BrandBook.Web.Framework/ViewModels/App/UserManagement/RoleViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandBook.Web.Framework.ViewModels.App.UserManagement
+{
+    public class RoleOverviewViewModel : IEnumerable<SingleRoleViewModel>
+    {
+        public List<SingleRoleViewModel> Roles { get; set; }
+
+        public IEnumerator<SingleRoleViewModel> GetEnumerator()
+        {
+            return Roles.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class SingleRoleViewModel
+    {
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
