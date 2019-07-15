cf0fae4247d2215fd9f6ccf34e3c94250b8df1e0
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-15 21:05:17 +0200

    Add Properties to BrandPublicSetting

--- BrandBook.Core/Domain/Brand/BrandPublicSetting.cs
+++ BrandBook.Core/Domain/Brand/BrandPublicSetting.cs
@@ -8,5 +8,10 @@ namespace BrandBook.Core.Domain.Brand
 {
     public class BrandPublicSetting : BaseEntity
     {
+
+        public bool IsPublic { get; set; }
+        public string ShareString { get; set; }
+
     }
+
 }
