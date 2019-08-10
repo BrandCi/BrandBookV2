50d1f6150dff46b0f2da23988901e1255f01a472
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-10 23:05:12 +0200

    ImageRepository: Implement IImageRepository

--- BrandBook.Infrastructure/Repositories/Resource/ImageRepository.cs
+++ BrandBook.Infrastructure/Repositories/Resource/ImageRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.Resource;
+using BrandBook.Core.Repositories.Resource;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories.Resource
 {
-    class ImageRepository
+    public class ImageRepository : Repository<Image>, IImageRepository
     {
+        public ImageRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
     }
 }
