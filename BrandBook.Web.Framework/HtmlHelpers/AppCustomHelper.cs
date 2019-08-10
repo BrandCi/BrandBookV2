d38e3a55edc3ea8753df0d0dbc349d12fdea36dd
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-10 13:21:31 +0200

    App: Link Button and Image for Brand Overview Items

--- BrandBook.Web.Framework/HtmlHelpers/AppCustomHelper.cs
+++ BrandBook.Web.Framework/HtmlHelpers/AppCustomHelper.cs
@@ -69,7 +69,10 @@ namespace BrandBook.Web.Framework.HtmlHelpers
                 html.Append("<div class=\"col-sm-6 col-lg-4 col-xs-12\">");
                 html.Append("<div class=\"card m-b-20\">");
 
+                html.Append("<a href=\"" + "/App/Brand/Index/" + brand.Id + "\">");
                 html.Append("<img class=\"card-img-top img-fluid\" src=\"" + contentSrv + "/" + appKey + "/" + "BrandData" + "/" + brand.Image + "\" alt=\"" + brand.Name + "\">");
+                html.Append("</a>");
+
                 html.Append("<div class=\"card-body\">");
                 html.Append("<h2 class=\"card-title\">" + brand.Name + "</h2>");
                 html.Append("<p class=\"card-text\">");
