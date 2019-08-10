28a030a606c7f2c7b206de6ebcd8bcdfc2470ae9
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-10 16:20:31 +0200

    Migrations: Add New Migration to Migrate UserLike Setting

---
+++ BrandBook.Infrastructure/Migrations/201908101418056_AddUserlikeSettingToSettingsTable.cs
@@ -0,0 +1,16 @@
+namespace BrandBook.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserlikeSettingToSettingsTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
