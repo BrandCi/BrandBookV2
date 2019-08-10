6e84ddb5d8128f9e3161bc45a365ba23402203d6
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-10 13:49:50 +0200

    Migrations: Add Migration to AddIsActivePropertyToAppUsersTable

---
+++ BrandBook.Infrastructure/Migrations/201908101148149_AddIsActivePropertyToAppUsersTable.cs
@@ -0,0 +1,18 @@
+namespace BrandBook.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsActivePropertyToAppUsersTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "IsActive", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "IsActive");
+        }
+    }
+}
