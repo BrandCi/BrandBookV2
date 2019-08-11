aca3067384ac86c0c9a40c9ebee937ea78f4e718
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-11 18:56:57 +0200

    ViewModel: Add Properties to AppCultureViewModel

--- BrandBook.Web.Framework/ViewModels/App/Settings/AppCultureViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Settings/AppCultureViewModel.cs
@@ -8,5 +8,7 @@ namespace BrandBook.Web.Framework.ViewModels.App.Settings
 {
     public class AppCultureViewModel
     {
+        public int AmountOfTranslations { get; set; }
+        public string SelectedCultureForExport { get; set; }
     }
 }
