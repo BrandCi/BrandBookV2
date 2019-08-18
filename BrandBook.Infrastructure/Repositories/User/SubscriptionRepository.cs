65aa809d5f5d4860204409ba7ab5b32fbeb05250
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 12:47:03 +0200

    Repository: Implement SubscriptionRepository

---
+++ BrandBook.Infrastructure/Repositories/User/SubscriptionRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrandBook.Core.Domain.User;
+using BrandBook.Core.Repositories.User;
+using BrandBook.Infrastructure.Data;
+
+namespace BrandBook.Infrastructure.Repositories.User
+{
+    public class SubscriptionRepository : Repository<Subscription>, ISubscriptionRepository
+    {
+        public SubscriptionRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
+
+
+    }
+}
