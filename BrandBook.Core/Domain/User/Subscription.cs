7e8d7d50a6887bf2c5fdfb87d0a84e3861825ff4
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 12:07:01 +0200

    Core: Add Properties to Subscription Model

--- BrandBook.Core/Domain/User/Subscription.cs
+++ BrandBook.Core/Domain/User/Subscription.cs
@@ -8,5 +8,15 @@ namespace BrandBook.Core.Domain.User
 {
     public class Subscription : BaseEntity
     {
+
+        public DateTime StartDateTime { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsActive { get; set; }
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+        public int SubscriptionPlanId { get; set; }
+        public SubscriptionPlan SubscriptionPlan { get; set; }
     }
 }
