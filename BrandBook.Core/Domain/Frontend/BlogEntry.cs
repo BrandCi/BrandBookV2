f5eb2e4eba6c4dc4ebdff29c1dedadc3f684250a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 21:19:45 +0200

    Blog: Adjust Namespaces for BlogEntry

--- BrandBook.Core/Domain/Frontend/BlogEntry.cs
+++ BrandBook.Core/Domain/Frontend/BlogEntry.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BrandBook.Core.Domain.Blog
+namespace BrandBook.Core.Domain.Frontend
 {
     public class BlogEntry : BaseEntity
     {
