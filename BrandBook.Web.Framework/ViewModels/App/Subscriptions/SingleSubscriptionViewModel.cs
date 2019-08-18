390d5c78c30aed87b4c6c299868adfe68b397888
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 13:40:00 +0200

    ViewModel: Add Properties to SingleSubscriptionViewModel

--- BrandBook.Web.Framework/ViewModels/App/Subscriptions/SingleSubscriptionViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Subscriptions/SingleSubscriptionViewModel.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Subscriptions
 {
-    class SingleSubscriptionViewModel
+    public class SingleSubscriptionViewModel
     {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsActive { get; set; }
+
+
+        // subscription plan
+        public string PlanName { get; set; }
+        public int PlanValidityInMonths { get; set; }
+        public double PlanPricePerMonth { get; set; }
     }
 }
--- BrandBook.Web.Framework/ViewModels/App/Subscriptions/SubscriptionsViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Subscriptions/SubscriptionsViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Subscriptions
 {
-    class SubscriptionsViewModel
+    public class SubscriptionsViewModel
     {
     }
 }
