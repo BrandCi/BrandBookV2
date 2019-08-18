d962d3fa346a670b0f4f4566500fb8fed34a6be7
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 16:15:11 +0200

    Service: Inject UnitOfWork in SubscriptionService

--- BrandBook.Services/Subscriptions/SubscriptionService.cs
+++ BrandBook.Services/Subscriptions/SubscriptionService.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core;
+using BrandBook.Infrastructure;
 
 namespace BrandBook.Services.Subscriptions
 {
     public class SubscriptionService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SubscriptionService()
+        {
+            this._unitOfWork = new UnitOfWork();
+        }
+
     }
 }
