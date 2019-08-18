3bfa37b53cc37cd9d153554c0151ffa7362ea9b5
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 14:19:03 +0200

    Repository: Add ISubscriptionPlanRepository

---
+++ BrandBook.Core/Repositories/User/ISubscriptionPlanRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrandBook.Core.Domain.User;
+
+namespace BrandBook.Core.Repositories.User
+{
+    public interface ISubscriptionPlanRepository : IRepository<SubscriptionPlan>
+    {
+    }
+}
