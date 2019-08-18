3a19a2fa673032ca8c45441a77807262f00ae32b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 21:20:44 +0200

    Repository: Add IBlogEntryRepository

---
+++ BrandBook.Core/Repositories/Frontend/IBlogEntryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrandBook.Core.Domain.Frontend;
+
+namespace BrandBook.Core.Repositories.Frontend
+{
+    public interface IBlogEntryRepository : IRepository<BlogEntry>
+    {
+    }
+}
