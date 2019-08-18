f9fce406231efde76c9e6f16b8f76441c2893327
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 16:35:17 +0200

    SubscriptionService: Add Method to check, if valid subscription is active

--- BrandBook.Services/Subscriptions/SubscriptionService.cs
+++ BrandBook.Services/Subscriptions/SubscriptionService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BrandBook.Core;
+using BrandBook.Core.Domain.User;
 using BrandBook.Infrastructure;
 
 namespace BrandBook.Services.Subscriptions
@@ -16,6 +17,41 @@ namespace BrandBook.Services.Subscriptions
         {
             this._unitOfWork = new UnitOfWork();
         }
+
+
+        public bool HasValidSubscription(string userId)
+        {
+            var subscriptions = _unitOfWork.SubscriptionRepository.GetActiveUserSubscriptions(userId);
+
+            foreach(var subscription in subscriptions)
+            {
+
+                if (IsEndDateInFuture(subscription))
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+        
+
+        private bool IsEndDateInFuture(Subscription subscription)
+        {
+            var subscriptionPlan = _unitOfWork.SubscriptionPlanRepository.FindById(subscription.SubscriptionPlanId);
+            var endDate = subscription.StartDateTime.AddMonths(subscriptionPlan.ValidityInMonths);
+
+            if (endDate > new DateTime())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
 
     }
 }
