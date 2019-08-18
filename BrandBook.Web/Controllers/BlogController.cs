283bb4c01aa3bbd58d11cf4f4962241d83fa5177
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 20:27:15 +0200

    Blog: Add Overview View

--- BrandBook.Web/Controllers/BlogController.cs
+++ BrandBook.Web/Controllers/BlogController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Controllers
 {
-    public class BlogController : Controller
+    public class BlogController : FrontendControllerBase
     {
         // GET: Blog
         public ActionResult Index()
