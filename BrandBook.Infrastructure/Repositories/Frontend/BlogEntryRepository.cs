b46fa68663ddc345af4350986cbbfbe0ddfa8cb1
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 21:22:02 +0200

    Repository: Implement IBlogEntryRepository in BlogEntryRepository

---
+++ BrandBook.Infrastructure/Repositories/Frontend/BlogEntryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrandBook.Core.Domain.Frontend;
+using BrandBook.Core.Repositories.Frontend;
+using BrandBook.Infrastructure.Data;
+
+namespace BrandBook.Infrastructure.Repositories.Frontend
+{
+    public class BlogEntryRepository : Repository<BlogEntry>, IBlogEntryRepository
+    {
+        public BlogEntryRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
