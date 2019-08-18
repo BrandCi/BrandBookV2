8cca70139d9b043eb6dc70d2202de7b79dea6f88
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 12:22:09 +0200

    Migrations: 201908181016148_SeedSubscriptionPlansTable

---
+++ BrandBook.Infrastructure/Migrations/201908181016148_SeedSubscriptionPlansTable.cs
@@ -0,0 +1,26 @@
+namespace BrandBook.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedSubscriptionPlansTable : DbMigration
+    {
+        public override void Up()
+        {
+
+            Sql("SET IDENTITY_INSERT [dbo].[SubscriptionPlans] ON");
+            Sql("INSERT INTO [dbo].[SubscriptionPlans] ([Id], [Name], [ValidityInMonths], [Price]) VALUES (1, N'Professional Monthly', 1, 4)");
+            Sql("INSERT INTO [dbo].[SubscriptionPlans] ([Id], [Name], [ValidityInMonths], [Price]) VALUES (2, N'Professional Annually', 12, 2)");
+            Sql("INSERT INTO [dbo].[SubscriptionPlans] ([Id], [Name], [ValidityInMonths], [Price]) VALUES (3, N'Business Monthly', 1, 7)");
+            Sql("INSERT INTO [dbo].[SubscriptionPlans] ([Id], [Name], [ValidityInMonths], [Price]) VALUES (4, N'Business Annually', 12, 5)");
+            Sql("INSERT INTO [dbo].[SubscriptionPlans] ([Id], [Name], [ValidityInMonths], [Price]) VALUES (5, N'Agency Monthly', 1, 25)");
+            Sql("INSERT INTO [dbo].[SubscriptionPlans] ([Id], [Name], [ValidityInMonths], [Price]) VALUES (6, N'Agency Annually', 12, 20)");
+            Sql("SET IDENTITY_INSERT [dbo].[SubscriptionPlans] OFF");
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
