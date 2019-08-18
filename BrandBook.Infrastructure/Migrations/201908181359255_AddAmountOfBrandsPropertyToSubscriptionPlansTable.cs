b422e1945eb02688a739883e9effbe39b3a771cd
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 16:04:14 +0200

    Migrations: Map AmountOfBrands with correct values

--- BrandBook.Infrastructure/Migrations/201908181359255_AddAmountOfBrandsPropertyToSubscriptionPlansTable.cs
+++ BrandBook.Infrastructure/Migrations/201908181359255_AddAmountOfBrandsPropertyToSubscriptionPlansTable.cs
@@ -8,6 +8,16 @@ namespace BrandBook.Infrastructure.Migrations
         public override void Up()
         {
             AddColumn("dbo.SubscriptionPlans", "AmountOfBrands", c => c.Int(nullable: false));
+
+            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 1 WHERE Id = 1;");
+            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 1 WHERE Id = 2;");
+            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 2 WHERE Id = 3;");
+            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 2 WHERE Id = 4;");
+            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 500 WHERE Id = 5;");
+            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 500 WHERE Id = 6;");
+
+
+
         }
         
         public override void Down()
