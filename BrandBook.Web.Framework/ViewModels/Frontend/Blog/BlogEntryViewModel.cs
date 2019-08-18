3e5bcb6fc70289442c05ea6928dd167f12bbb11d
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 21:40:08 +0200

    ViewModel: Add Properties to BlogEntryViewModel

--- BrandBook.Web.Framework/ViewModels/Frontend/Blog/BlogEntryViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/Frontend/Blog/BlogEntryViewModel.cs
@@ -8,5 +8,15 @@ namespace BrandBook.Web.Framework.ViewModels.Frontend.Blog
 {
     public class BlogEntryViewModel
     {
+
+        public string UrlKey { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Image { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime CreationDateTime { get; set; }
+
+
     }
 }
