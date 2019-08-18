90356a8b929ad864932dc59fbe98cb31a53ac24c
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 21:13:41 +0200

    Blog: Add Properties to BlogEntry DomainModel

--- BrandBook.Core/Domain/Blog/BlogEntry.cs
+++ BrandBook.Core/Domain/Blog/BlogEntry.cs
@@ -8,5 +8,15 @@ namespace BrandBook.Core.Domain.Blog
 {
     public class BlogEntry : BaseEntity
     {
+
+        public string UrlKey { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Image { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime CreationDateTime { get; set; }
+
+
     }
 }
