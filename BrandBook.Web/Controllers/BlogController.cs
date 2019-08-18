c3d3bd594616a24bb20a2bc208c6c4cf1dc4b7ab
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 21:34:23 +0200

    Blog: Add Views - dynamic and static

--- BrandBook.Web/Controllers/BlogController.cs
+++ BrandBook.Web/Controllers/BlogController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Core.Repositories.Frontend;
+using BrandBook.Infrastructure.Data;
+using BrandBook.Infrastructure.Repositories.Frontend;
 using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Controllers
 {
     public class BlogController : FrontendControllerBase
     {
+        private readonly IBlogEntryRepository _blogEntryRepository;
+
+        public BlogController()
+        {
+            _blogEntryRepository = new BlogEntryRepository( new BrandBookDbContext() );
+        }
+
+
         // GET: Blog
-        public ActionResult Index()
+        public ActionResult Index(string blogName = "")
+        {
+            return View();
+        }
+
+
+        public ActionResult Overview()
         {
             return View();
         }
