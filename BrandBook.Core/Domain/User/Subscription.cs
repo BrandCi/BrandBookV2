7cb1476707aaa861989eca122fb47e83b58fcc1c
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 12:02:36 +0200

    Core: Add new DomainModel Subscription
    Handle all subscription

---
+++ BrandBook.Core/Domain/User/Subscription.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandBook.Core.Domain.User
+{
+    public class Subscription : BaseEntity
+    {
+    }
+}
