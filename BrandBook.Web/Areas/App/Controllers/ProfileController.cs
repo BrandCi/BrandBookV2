06381acbd13505b12ac374f684cef988e771759b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-18 13:48:36 +0200

    ProfileController: Get all Subscriptions and Map to ViewModel

--- BrandBook.Web/Areas/App/Controllers/ProfileController.cs
+++ BrandBook.Web/Areas/App/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using BrandBook.Core;
 using BrandBook.Infrastructure;
 using BrandBook.Web.Framework.Controllers;
 using BrandBook.Web.Framework.ViewModels.App.Profile;
+using BrandBook.Web.Framework.ViewModels.App.Subscriptions;
 using Microsoft.AspNet.Identity;
 
 namespace BrandBook.Web.Areas.App.Controllers
@@ -10,16 +11,16 @@ namespace BrandBook.Web.Areas.App.Controllers
     public class ProfileController : AppControllerBase
     {
 
-        private readonly IUnitOfWork unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
 
         public ProfileController()
         {
-            this.unitOfWork = new UnitOfWork();
+            this._unitOfWork = new UnitOfWork();
         }
 
         public ActionResult Index()
         {
-            var appUser = unitOfWork.AppUserRepository.FindById(User.Identity.GetUserId());
+            var appUser = _unitOfWork.AppUserRepository.FindById(User.Identity.GetUserId());
 
             var model = new GeneralUserDataViewModel()
             {
@@ -42,13 +43,13 @@ namespace BrandBook.Web.Areas.App.Controllers
                 return RedirectToAction("Index", "Profile", new { area = "App" });
             }
 
-            var appUser = unitOfWork.AppUserRepository.FindById(User.Identity.GetUserId());
+            var appUser = _unitOfWork.AppUserRepository.FindById(User.Identity.GetUserId());
 
             appUser.FirstName = model.FirstName;
             appUser.LastName = model.LastName;
 
-            unitOfWork.AppUserRepository.Update(appUser);
-            unitOfWork.SaveChanges();
+            _unitOfWork.AppUserRepository.Update(appUser);
+            _unitOfWork.SaveChanges();
 
 
             return RedirectToAction("Index", "Profile", new {area = "App"});
@@ -59,7 +60,27 @@ namespace BrandBook.Web.Areas.App.Controllers
 
         public ActionResult Subscriptions()
         {
-            return View();
+            var subscriptions = _unitOfWork.SubscriptionRepository.GetAllUserSubscriptions(User.Identity.GetUserId());
+            var viewModel = new SubscriptionsViewModel();
+
+
+            foreach (var subscription in subscriptions)
+            {
+                viewModel.Subscriptions.Add(new SingleSubscriptionViewModel()
+                    {
+                        Id = subscription.Id,
+                        Key = subscription.Id.ToString(),
+                        IsActive = subscription.IsActive,
+                        IsPaid = subscription.IsPaid,
+                        PlanName = subscription.SubscriptionPlan.Name,
+                        PlanValidityInMonths = subscription.SubscriptionPlan.ValidityInMonths,
+                        PlanPricePerMonth = subscription.SubscriptionPlan.PricePerMonth
+                    }
+                );
+            }
+
+
+            return View(viewModel);
         }
 
 
