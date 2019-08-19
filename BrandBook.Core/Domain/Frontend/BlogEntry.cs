c9eec08c6db71f673f63e533ed62e0e8642a8fa0
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-19 22:12:56 +0200

    Core: Add IsPublished Property to BlogEntry

--- BrandBook.Core/Domain/Frontend/BlogEntry.cs
+++ BrandBook.Core/Domain/Frontend/BlogEntry.cs
@@ -4,7 +4,7 @@ namespace BrandBook.Core.Domain.Frontend
 {
     public class BlogEntry : BaseEntity
     {
-
+        public bool IsPublished { get; set; }
         public string UrlKey { get; set; }
         public string Title { get; set; }
         public string SubTitle { get; set; }
@@ -12,7 +12,5 @@ namespace BrandBook.Core.Domain.Frontend
         public string Content { get; set; }
         public string Author { get; set; }
         public DateTime CreationDateTime { get; set; }
-
-
     }
 }
