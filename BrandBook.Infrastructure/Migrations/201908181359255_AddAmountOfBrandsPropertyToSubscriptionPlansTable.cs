3a271593b9adf7eee5742cada85d7cf25c488aa2
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-19 21:09:52 +0200

    Migrations: Update Amount of Brand for Agency SubscriptionPlans

--- BrandBook.Infrastructure/Migrations/201908181359255_AddAmountOfBrandsPropertyToSubscriptionPlansTable.cs
+++ BrandBook.Infrastructure/Migrations/201908181359255_AddAmountOfBrandsPropertyToSubscriptionPlansTable.cs
@@ -13,8 +13,8 @@ namespace BrandBook.Infrastructure.Migrations
             Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 1 WHERE Id = 2;");
             Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 2 WHERE Id = 3;");
             Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 2 WHERE Id = 4;");
-            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 500 WHERE Id = 5;");
-            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 500 WHERE Id = 6;");
+            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 999 WHERE Id = 5;");
+            Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 999 WHERE Id = 6;");
             Sql("UPDATE dbo.SubscriptionPlans SET AmountOfBrands = 1 WHERE Id = 7;");
 
 
