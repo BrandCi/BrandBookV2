1d68e8ac9d5353c46814d076d22f86569bca592b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-19 22:19:28 +0200

    IRepository: Update IBlogEntryRepository

--- BrandBook.Core/Repositories/Frontend/IBlogEntryRepository.cs
+++ BrandBook.Core/Repositories/Frontend/IBlogEntryRepository.cs
@@ -12,7 +12,7 @@ namespace BrandBook.Core.Repositories.Frontend
 
         bool BlogEntryExists(string blogEntryKey);
         BlogEntry FindBlogEntryByKey(string blogEntryKey);
-        List<BlogEntry> GetAllPublishedBlogEntries();
+        Task<List<BlogEntry>> GetAllPublishedBlogEntriesAsync();
 
     }
 }
