d422699d49cbdbdd987f1ae40b44f1ba660cb86b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-20 21:59:17 +0200

    Migrations: 201908201957006_AddAuthorSettingKeyToSettingsTable

---
+++ BrandBook.Infrastructure/Migrations/201908201957006_AddAuthorSettingKeyToSettingsTable.cs
@@ -0,0 +1,19 @@
+namespace BrandBook.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAuthorSettingKeyToSettingsTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("SET IDENTITY_INSERT[dbo].[Settings] ON");
+            Sql("INSERT INTO[dbo].[Settings] ([Id], [Key], [Value], [Category], [AccessLevel]) VALUES(18, N'conf_system_appauthor', N'Philipp C. Moser', 0, 1)");
+            Sql("SET IDENTITY_INSERT[dbo].[Settings] OFF");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
