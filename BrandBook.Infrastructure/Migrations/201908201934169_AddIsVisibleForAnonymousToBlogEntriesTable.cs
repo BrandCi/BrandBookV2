d31c8436306d99591c8771e214d35595cfefadd0
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-20 21:34:42 +0200

    Migrations: 201908201934169_AddIsVisibleForAnonymousToBlogEntriesTable

---
+++ BrandBook.Infrastructure/Migrations/201908201934169_AddIsVisibleForAnonymousToBlogEntriesTable.cs
@@ -0,0 +1,18 @@
+namespace BrandBook.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsVisibleForAnonymousToBlogEntriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.BlogEntries", "IsVisibleForAnonymous", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.BlogEntries", "IsVisibleForAnonymous");
+        }
+    }
+}
