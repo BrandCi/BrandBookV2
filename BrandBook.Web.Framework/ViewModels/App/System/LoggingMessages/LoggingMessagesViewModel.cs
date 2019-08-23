c695c0b243bf6f413b86bee02b3d58601c358791
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-23 21:34:31 +0200

    ViewModel: Implement IEnumerable in LoggingMessageViewModel

---
+++ BrandBook.Web.Framework/ViewModels/App/System/LoggingMessages/LoggingMessagesViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandBook.Web.Framework.ViewModels.App.System.LoggingMessages
+{
+    public class LoggingMessagesViewModel : IEnumerable<SingleLoggingMessageViewModel>
+    {
+        public IEnumerable<SingleLoggingMessageViewModel> LoggingMessages { get; set; }
+
+        public IEnumerator<SingleLoggingMessageViewModel> GetEnumerator()
+        {
+            return LoggingMessages.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+    }
+}
