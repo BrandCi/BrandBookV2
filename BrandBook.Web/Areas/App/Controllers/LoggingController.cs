60bc7cf822c03c2d0c9830ae3b6ff497a312555a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-23 21:29:28 +0200

    App: Add new LoggingController

---
+++ BrandBook.Web/Areas/App/Controllers/LoggingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
+
+namespace BrandBook.Web.Areas.App.Controllers
+{
+    public class LoggingController : AppControllerBase
+    {
+        // GET: App/Logging
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
