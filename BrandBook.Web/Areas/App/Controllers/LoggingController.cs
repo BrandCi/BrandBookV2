3b9f93a4cabbcc8d766ad270f6b225458f61f269
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-23 21:36:07 +0200

    LoggingController: Implement IUnitOfWork

--- BrandBook.Web/Areas/App/Controllers/LoggingController.cs
+++ BrandBook.Web/Areas/App/Controllers/LoggingController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrandBook.Core;
+using BrandBook.Infrastructure;
 using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Areas.App.Controllers
 {
     public class LoggingController : AppControllerBase
     {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LoggingController()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+
+
         // GET: App/Logging
         public ActionResult LoggingMessages()
         {
+          
+
+
+            
             return View();
         }
     }
