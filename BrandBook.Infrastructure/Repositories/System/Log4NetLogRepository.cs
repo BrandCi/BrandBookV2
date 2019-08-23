9014ccb0c034b71c1a3307511401eb754c5adfbc
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-23 21:25:33 +0200

    Log4NetLogRepository: Implement ILog4NetLogRepository

--- BrandBook.Infrastructure/Repositories/System/Log4NetLogRepository.cs
+++ BrandBook.Infrastructure/Repositories/System/Log4NetLogRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.System;
+using BrandBook.Core.Repositories.System;
+using BrandBook.Infrastructure.Data;
 
 namespace BrandBook.Infrastructure.Repositories.System
 {
-    class Log4NetLogRepository
+    public class Log4NetLogRepository : Repository<Log4NetLog>, ILog4NetLogRepository
     {
+        public Log4NetLogRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
     }
 }
