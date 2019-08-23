bb1dd8618382cf71644a74976b574c33bedc2a1f
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-23 21:24:02 +0200

    IRepository: Implement IRepository

--- BrandBook.Core/Repositories/System/ILog4NetLogRepository.cs
+++ BrandBook.Core/Repositories/System/ILog4NetLogRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Core.Domain.System;
 
 namespace BrandBook.Core.Repositories.System
 {
-    interface ILog4NetLogRepository
+    public interface ILog4NetLogRepository : IRepository<Log4NetLog>
     {
     }
 }
