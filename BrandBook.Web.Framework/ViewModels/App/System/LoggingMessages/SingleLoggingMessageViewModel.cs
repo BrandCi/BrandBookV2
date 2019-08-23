3ac2e5d6b298896b5b1c435aed7fab2058acf345
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-23 21:33:01 +0200

    ViewModel: Add Properties to SingleLoggingMessageViewModel

--- BrandBook.Web.Framework/ViewModels/App/System/LoggingMessages/SingleLoggingMessageViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/System/LoggingMessages/SingleLoggingMessageViewModel.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.ViewModels.App.System.LoggingMessages
 {
-    class SingleLoggingMessageViewModel
+    public class SingleLoggingMessageViewModel
     {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Thread { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
     }
 }
