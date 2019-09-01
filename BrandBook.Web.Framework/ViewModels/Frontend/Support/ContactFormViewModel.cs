ad014f11a48f3874bc37ae89f915f4fcd2bf5061
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-01 12:05:16 +0200

    ViewModel: Add Values and DataAnnotations to ContactFormViewModel

--- BrandBook.Web.Framework/ViewModels/Frontend/Support/ContactFormViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/Frontend/Support/ContactFormViewModel.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrandBook.Resources;
 
 namespace BrandBook.Web.Framework.ViewModels.Frontend.Support
 {
-    class ContactFormViewModel
+    public class ContactFormViewModel
     {
+        [Display(Name = "frontend_contact_input_name", ResourceType = typeof(Translations))]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "frontend_contact_input_email", ResourceType = typeof(Translations))]
+        public string Email { get; set; }
+
+        [Display(Name = "frontend_contact_input_subject", ResourceType = typeof(Translations))]
+        public string Subject { get; set; }
+
+        [Display(Name = "frontend_contact_input_message", ResourceType = typeof(Translations))]
+        public string Message { get; set; }
+
+        [Range(typeof(bool), "true", "true")]
+        [Display(Name = "frontend_contact_input_privacypolicy", ResourceType = typeof(Translations))]
+        public bool PrivacyPolicy { get; set; }
+
     }
 }
