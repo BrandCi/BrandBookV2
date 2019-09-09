96cd3c086e913e85ac8bd3f53a12dd084e5d06b5
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-09 14:35:24 +0200

    Translations: Implement Translations

--- BrandBook.Web.Framework/ViewModels/App/Brand/AddNewBrandViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Brand/AddNewBrandViewModel.cs
@@ -1,18 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using BrandBook.Resources;
 
 namespace BrandBook.Web.Framework.ViewModels.App.Brand
 {
     public class AddNewBrandViewModel
     {
         [Required]
+        [Display(Name = "app_brands_addbrand_name", ResourceType = typeof(Translations))]
         public string Name { get; set; }
 
+        [Display(Name = "app_brands_addbrand_description", ResourceType = typeof(Translations))]
         public string Description { get; set; }
+
+        [Display(Name = "app_brands_addbrand_shortdescription", ResourceType = typeof(Translations))]
         public string ShortDescription { get; set; }
 
+        [Display(Name = "app_brands_addbrand_maincolor", ResourceType = typeof(Translations))]
         public string MainColor { get; set; }
 
-        [Display(Name="Brand Image")]
+        [Display(Name = "app_brands_addbrand_image", ResourceType = typeof(Translations))]
         public int ImageId { get; set; }
 
     }
