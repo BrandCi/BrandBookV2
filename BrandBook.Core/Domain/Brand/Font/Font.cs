eef8ef9f81f6758df27477c784399ced794b9a6c
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-11 22:46:17 +0200

    Core: Updated Font DomainModel

--- BrandBook.Core/Domain/Brand/Font/Font.cs
+++ BrandBook.Core/Domain/Brand/Font/Font.cs
@@ -11,6 +11,9 @@ namespace BrandBook.Core.Domain.Brand.Font
         public string Name { get; set; }
         public string Family { get; set; }
 
+        public int FontInclusionId { get; set; }
+        public FontInclusion FontInclusion { get; set; }
+
         public int BrandId { get; set; }
         public Brand Brand { get; set; }
 
