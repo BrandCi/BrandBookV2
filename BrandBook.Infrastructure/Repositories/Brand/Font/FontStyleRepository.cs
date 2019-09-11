a23d5a2f96ccb177d5c5dc7fe974b2d872269366
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-11 22:35:10 +0200

    Repository: Add FontStyleRepository

---
+++ BrandBook.Infrastructure/Repositories/Brand/Font/FontStyleRepository.cs
@@ -0,0 +1,14 @@
+﻿using BrandBook.Core.Domain.Brand.Font;
+using BrandBook.Core.Repositories.Brand.Font;
+using BrandBook.Infrastructure.Data;
+
+namespace BrandBook.Infrastructure.Repositories.Brand.Font
+{
+    public class FontStyleRepository : Repository<FontStyle>, IFontStyleRepository
+    {
+        public FontStyleRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
