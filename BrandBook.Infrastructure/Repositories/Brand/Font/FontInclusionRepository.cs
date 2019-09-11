9ee31c962240bc67667747d0f265af3718a4be44
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-11 22:34:04 +0200

    Repository: Add FontInclusionRepository

---
+++ BrandBook.Infrastructure/Repositories/Brand/Font/FontInclusionRepository.cs
@@ -0,0 +1,14 @@
+﻿using BrandBook.Core.Domain.Brand.Font;
+using BrandBook.Core.Repositories.Brand.Font;
+using BrandBook.Infrastructure.Data;
+
+namespace BrandBook.Infrastructure.Repositories.Brand.Font
+{
+    public class FontInclusionRepository : Repository<FontInclusion>, IFontInclusionRepository
+    {
+        public FontInclusionRepository(BrandBookDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
