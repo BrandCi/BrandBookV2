136a7c600ece35ba89b6ce8a5a7df942944b72f8
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-11 23:07:28 +0200

    ViewModel: Add SingleFontViewModel

---
+++ BrandBook.Web.Framework/ViewModels/App/Brand/Fonts/SingleFontViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandBook.Web.Framework.ViewModels.App.Brand.Fonts
+{
+    public class SingleFontViewModel : IEnumerable<FontStyleViewModel>
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public FontInclusionViewModel FontInclusion { get; set; }
+
+        public List<FontStyleViewModel> FontStyles { get; set; }
+
+        public IEnumerator<FontStyleViewModel> GetEnumerator()
+        {
+            return FontStyles.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+    }
+}
