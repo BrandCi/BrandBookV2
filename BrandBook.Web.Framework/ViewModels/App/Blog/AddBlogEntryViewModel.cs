6ddb4c872936e23478c6b5e2bf720b2a437cfb84
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-29 14:01:46 +0200

    Updated Add View for BlogEntries

--- BrandBook.Web.Framework/ViewModels/App/Blog/AddBlogEntryViewModel.cs
+++ BrandBook.Web.Framework/ViewModels/App/Blog/AddBlogEntryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace BrandBook.Web.Framework.ViewModels.App.Blog
 {
     public class AddBlogEntryViewModel
     {
+        [Required]
         public string Title { get; set; }
         public string SubTitle { get; set; }
 
