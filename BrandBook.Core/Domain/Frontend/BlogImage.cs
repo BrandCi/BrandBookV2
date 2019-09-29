a84cf4a84c1deda6f956714e7919bfd321072672
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-29 13:17:14 +0200

    Update BlogImage.cs

--- BrandBook.Core/Domain/Frontend/BlogImage.cs
+++ BrandBook.Core/Domain/Frontend/BlogImage.cs
@@ -8,5 +8,7 @@ namespace BrandBook.Core.Domain.Frontend
 {
     public class BlogImage : BaseEntity
     {
+        public string Name { get; set; }
+        public string ContentType { get; set; }
     }
 }
