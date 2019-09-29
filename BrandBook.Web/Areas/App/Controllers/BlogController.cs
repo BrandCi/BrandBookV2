1e8ac57b3ec6673ca6c79d9d0abb81f73068eca4
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-29 12:41:37 +0200

    Create new BlogController for App Area

---
+++ BrandBook.Web/Areas/App/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrandBook.Web.Framework.Controllers;
+
+namespace BrandBook.Web.Areas.App.Controllers
+{
+    public class BlogController : AppControllerBase
+    {
+        // GET: App/Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
