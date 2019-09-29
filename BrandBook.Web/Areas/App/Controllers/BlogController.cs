c884852d55bedd1168f393e4db4ebdd4526c7c23
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-29 12:54:26 +0200

    Inject UnitOfWork in BlogController

--- BrandBook.Web/Areas/App/Controllers/BlogController.cs
+++ BrandBook.Web/Areas/App/Controllers/BlogController.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using BrandBook.Core;
+using BrandBook.Infrastructure;
 using BrandBook.Web.Framework.Controllers;
 
 namespace BrandBook.Web.Areas.App.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class BlogController : AppControllerBase
     {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BlogController()
+        {
+            this._unitOfWork = new UnitOfWork();
+        }
+
+
         // GET: App/Blog
         public ActionResult Index()
         {
