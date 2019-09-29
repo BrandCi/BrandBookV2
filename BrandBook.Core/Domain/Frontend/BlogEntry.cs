cc9ff5173fa595841753dea96082c1198dba2325
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-29 13:20:40 +0200

    Remove currently existing Blog.Image

--- BrandBook.Core/Domain/Frontend/BlogEntry.cs
+++ BrandBook.Core/Domain/Frontend/BlogEntry.cs
@@ -10,7 +10,6 @@ namespace BrandBook.Core.Domain.Frontend
         public string UrlKey { get; set; }
         public string Title { get; set; }
         public string SubTitle { get; set; }
-        public string Image { get; set; }
         public string AdditionalStyles { get; set; }
         public string Content { get; set; }
         public string Author { get; set; }
