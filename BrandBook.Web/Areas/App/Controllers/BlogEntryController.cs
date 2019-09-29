ffa6e7a146b9a24e57c7177e902835f251cd287f
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-09-29 14:22:08 +0200

    BlogEntryController: Finalizing the creation of new BlogEntries

--- BrandBook.Web/Areas/App/Controllers/BlogEntryController.cs
+++ BrandBook.Web/Areas/App/Controllers/BlogEntryController.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using BrandBook.Core;
+using BrandBook.Core.Domain.Frontend;
 using BrandBook.Infrastructure;
 using BrandBook.Web.Framework.Controllers;
 using BrandBook.Web.Framework.ViewModels.App.Blog;
 using BrandBook.Web.Framework.ViewModels.App.Blog.Overview;
+using Microsoft.AspNet.Identity;
 
 namespace BrandBook.Web.Areas.App.Controllers
 {
@@ -55,6 +59,57 @@ namespace BrandBook.Web.Areas.App.Controllers
             var viewModel = new AddBlogEntryViewModel();
 
             return View(viewModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(AddBlogEntryViewModel model)
+        {
+
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var blogEntry = new BlogEntry()
+            {
+                Title = model.Title,
+                SubTitle = model.SubTitle,
+                Content = model.Content,
+                AdditionalStyles = model.AdditionalStyles,
+                Author = model.Author,
+
+                IsPublished = false,
+                IsVisibleForAnonymous = false,
+                UrlKey = GenerateUrlKey(),
+                CreationDateTime = DateTime.Now,
+
+                BlogImageId = 1,
+                AppUserId = User.Identity.GetUserId()
+
+            };
+
+            _unitOfWork.BlogEntryRepository.Add(blogEntry);
+            _unitOfWork.SaveChanges();
+
+
+            return RedirectToAction("Index", "BlogEntry", new { area="App" });
+
+        }
+
+
+
+
+        private string GenerateUrlKey()
+        {
+            var random = new Random();
+
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+            return new string(Enumerable.Repeat(chars, 20)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray());
         }
 
 
