0ba708202425d7aaee8348f29d56a59a9e930b77
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-29 14:03:52 +0100

    Resorted the BlogEntry Model Properties

--- BrandBook.Core/Domain/Frontend/BlogEntry.cs
+++ BrandBook.Core/Domain/Frontend/BlogEntry.cs
@@ -5,11 +5,11 @@ namespace BrandBook.Core.Domain.Frontend
 {
     public class BlogEntry : BaseEntity
     {
-        public bool IsPublished { get; set; }
-        public bool IsVisibleForAnonymous { get; set; }
-        public string UrlKey { get; set; }
         public string Title { get; set; }
         public string SubTitle { get; set; }
+        public string UrlKey { get; set; }
+        public bool IsPublished { get; set; }
+        public bool IsVisibleForAnonymous { get; set; }
         public string AdditionalStyles { get; set; }
         public string Content { get; set; }
         public string Author { get; set; }
