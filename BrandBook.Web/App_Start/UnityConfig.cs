3e65640a58affa4b40604c063791953acbd6d1e6
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-29 15:46:58 +0100

    Registered first Type in UnityContainer

--- BrandBook.Web/App_Start/UnityConfig.cs
+++ BrandBook.Web/App_Start/UnityConfig.cs
@@ -2,6 +2,12 @@ using System.Web.Mvc;
 using Unity;
 using Unity.Mvc5;
 
+using BrandBook.Core;
+using BrandBook.Infrastructure;
+
+
+
+
 namespace BrandBook.Web
 {
     public static class UnityConfig
@@ -9,11 +15,9 @@ namespace BrandBook.Web
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
+
+
+            container.RegisterType<IUnitOfWork, UnitOfWork>();
             
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
