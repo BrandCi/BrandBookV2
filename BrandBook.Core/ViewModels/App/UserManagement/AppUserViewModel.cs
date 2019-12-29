a564b5d3e0ec6478c6b5ed59af25fe3fba9899e5
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-29 17:02:14 +0100

    Added SingleAppUserViewModel to separate file

--- BrandBook.Core/ViewModels/App/UserManagement/AppUserViewModel.cs
+++ BrandBook.Core/ViewModels/App/UserManagement/AppUserViewModel.cs
@@ -20,13 +20,4 @@ namespace BrandBook.Core.ViewModels.App.UserManagement
         }
     }
 
-    public class SingleAppUserViewModel
-    {
-
-        public string Email { get; set; }
-        public string UserName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-    }
 }
