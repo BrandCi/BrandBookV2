da0ffe0bb42888e9186b093557d6d89ada94d397
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-29 14:16:07 +0100

    Move BlogDto to Subfolder and add Properties

---
+++ BrandBook.Core/Dto/Frontend/Blog/BlogDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandBook.Core.Dto.Frontend.Blog
+{
+    public class BlogDto
+    {
+        public string UrlKey { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
