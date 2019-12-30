85a0c136421332d0529060b6cf780ed30e997d75
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-30 21:01:31 +0100

    Add Api/Blogs BlogsController to provide the BlogData requested. (based on authentication)

--- BrandBook.Web/Api/Frontend/BlogsController.cs
+++ BrandBook.Web/Api/Frontend/BlogsController.cs
@@ -1,10 +1,42 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using AutoMapper;
 
+using BrandBook.Core;
+using BrandBook.Infrastructure;
+using BrandBook.Core.Domain.Frontend;
+using BrandBook.Core.Dto.Frontend.Blog;
+using System.Threading.Tasks;
 
 namespace BrandBook.Web.Api.Frontend
 {
     public class BlogsController : ApiController
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BlogsController()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+
         
+        public async Task<IEnumerable<BlogDto>> GetAll()
+        {
+            var allBlogs = await _unitOfWork.BlogEntryRepository.GetAllPublishedBlogEntriesForAnonymousUserAsync(); ;
+
+            if(User.Identity.IsAuthenticated)
+            {
+                allBlogs = await _unitOfWork.BlogEntryRepository.GetAllPublishedBlogEntriesAsync();
+            }
+
+
+            return Mapper.Map<IEnumerable<BlogEntry>, IEnumerable<BlogDto>>(allBlogs);
+
+        }
+        
+
+
     }
 }
