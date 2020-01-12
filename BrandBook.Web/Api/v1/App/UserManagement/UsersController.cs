6ff6fffea4af054a6c7966c1973c0b3e75b10b77
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-12 19:58:13 +0100

    First Call to get All Users

--- BrandBook.Web/Api/v1/App/UserManagement/UsersController.cs
+++ BrandBook.Web/Api/v1/App/UserManagement/UsersController.cs
@@ -1,10 +1,14 @@
-﻿using BrandBook.Core;
+﻿using AutoMapper;
+using BrandBook.Core;
+using BrandBook.Core.Domain.User;
+using BrandBook.Core.Dto.App.UserManagement;
 using BrandBook.Web.Framework.Controllers.ApiControllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace BrandBook.Web.Api.v1.App.UserManagement
@@ -21,8 +25,16 @@ namespace BrandBook.Web.Api.v1.App.UserManagement
         }
 
 
+        [Route("")]
+        public async Task<IEnumerable<UserDto>> GetAll()
+        {
+            var allUsers = await _unitOfWork.AppUserRepository.GetAllAsync();
 
-  
+            return Mapper.Map<IEnumerable<AppUser>, IEnumerable<UserDto>>(allUsers);
+
+        }
+
+
 
 
     }
