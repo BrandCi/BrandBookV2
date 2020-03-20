5a8118e90569d65e367166f871f0fb672d3e942e
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-03-20 23:35:34 +0100

    CHANGED: BB #81
    Add new SubscriptionsController (Api)

---
+++ BrandBook.Web/Api/v1/App/UserManagement/SubscriptionsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using BrandBook.Core;
+using BrandBook.Core.Domain.User;
+using BrandBook.Core.Dto.App.UserManagement;
+using BrandBook.Web.Framework.Controllers.ApiControllers;
+
+namespace BrandBook.Web.Api.v1.App.UserManagement
+{
+    [RoutePrefix("api/v1/subscriptions")]
+    public class SubscriptionsController : AppApiControllerBase
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SubscriptionsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+    }
+}
