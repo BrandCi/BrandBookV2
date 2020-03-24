f4f090fcc5431902d95d8377dc9afef3bbc45c1a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-03-24 22:25:35 +0100

    CHANGED: BB #94
    Added new IEmailBuilder and EmailBuilder

---
+++ BrandBook.Services/Notification/EmailBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrandBook.Core.Services.Notification;
+
+namespace BrandBook.Services.Notification
+{
+    public class EmailBuilder : IEmailBuilder
+    {
+    }
+}
