253157e93ece23904c1106d27d7bd8fb66484009
Philipp Moser <info@philipp-moser.de>
2020-05-01 12:59:47 +0200

    BB-159: Register StorageService in Unity Container

--- BrandBook.Web/App_Start/UnityConfig.cs
+++ BrandBook.Web/App_Start/UnityConfig.cs
@@ -5,11 +5,13 @@ using Unity.WebApi;
 using BrandBook.Core;
 using BrandBook.Core.Services.Authentication;
 using BrandBook.Core.Services.Messaging;
+using BrandBook.Core.Services.Storage;
 using BrandBook.Infrastructure;
 
 using BrandBook.Core.Services.Subscriptions;
 using BrandBook.Services.Authentication;
 using BrandBook.Services.Notification;
+using BrandBook.Services.Storage;
 using BrandBook.Services.Subscriptions;
 
 namespace BrandBook.Web
@@ -28,6 +30,7 @@ namespace BrandBook.Web
             container.RegisterType<ISubscriptionService, SubscriptionService>();
             container.RegisterType<IReCaptchaService, ReCaptchaService>();
             container.RegisterType<INotificationService, NotificationService>();
+            container.RegisterType<IStorageService, BlobStorageService>();
             #endregion
 
             
