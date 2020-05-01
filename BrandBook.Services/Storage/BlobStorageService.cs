98816ab60a5dc910f55fa174f37ce93c1003ca9a
Philipp Moser <info@philipp-moser.de>
2020-05-01 12:43:58 +0200

    BB-159: Create new BlobStorageService
    Implements IStorageService

---
+++ BrandBook.Services/Storage/BlobStorageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrandBook.Core.Services.Storage;
+
+namespace BrandBook.Services.Storage
+{
+    public class BlobStorageService : IStorageService
+    {
+    }
+}
