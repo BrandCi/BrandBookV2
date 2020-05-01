55b2faf698102c3104aab60c45fb1cb6c9396adc
Philipp Moser <info@philipp-moser.de>
2020-05-01 13:41:20 +0200

    BB-159: Implement Authentication for Azure Storage Account

--- BrandBook.Services/Storage/BlobStorageService.cs
+++ BrandBook.Services/Storage/BlobStorageService.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using BrandBook.Core.Services.Storage;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
 
 namespace BrandBook.Services.Storage
 {
     public class BlobStorageService : IStorageService
     {
+        #region Fields
+        private readonly BlobContainerClient _blobServiceClient;
+        #endregion
+
+
+        public BlobStorageService()
+        {
+            var storageConnectionString = ConfigurationManager.ConnectionStrings["BlobStorageConnection"].ToString();
+            var containerName = ConfigurationManager.AppSettings["BlobStorageContainer"];
+
+            _blobServiceClient = new BlobContainerClient(storageConnectionString, containerName);
+
+            _blobServiceClient.CreateIfNotExists();
+
+        }
+
     }
 }
