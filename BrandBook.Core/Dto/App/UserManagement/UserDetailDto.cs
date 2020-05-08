6e72195cedb823eb12689ae762a8a4bf2e0f32c4
Philipp Moser <info@philipp-moser.de>
2020-05-08 22:06:12 +0200

    BB-171: Created new UserDetailDto

---
+++ BrandBook.Core/Dto/App/UserManagement/UserDetailDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandBook.Core.Dto.App.UserManagement
+{
+    public class UserDetailDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string PhoneNumber { get; set; }
+        public int IsActive { get; set; }
+
+        public string CreationDate { get; set; }
+        public string LastLoginDate { get; set; }
+        public string LastModifiedDate { get; set; }
+
+        public int IsEmailConfirmed { get; set; }
+        public int TwoFactorEnabled { get; set; }
+        public int LockoutEnabled { get; set; }
+        public int PrivacyPolicyAccepted { get; set; }
+        public int IsDarkmodeEnabled { get; set; }
+    }
+}
