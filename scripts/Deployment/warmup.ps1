8c44cac7d9c3b70cac673a66c1ccd96a7fd30988
Philipp Moser <info@philipp-moser.de>
2020-07-05 19:07:51 +0200

    BB-220: Add some optimized handling and user feedback

--- scripts/Deployment/warmup.ps1
+++ scripts/Deployment/warmup.ps1
@@ -15,9 +15,33 @@ Foreach ($url IN $urls)
 {
     $completeUrl = $baseUrl + $url
     $response = Invoke-WebRequest -Uri $completeUrl -Method Get
-    "Url: " + $url + "`n" + "Status: " + $response.StatusCode + "`n`n"
+
+    $statusCode = $response.StatusCode
+    $statusCodeGroup = $statusCode.ToString().substring(0,1)
+
+
+    $status = 'Yellow'
+    if($statusCodeGroup -eq '2') {
+        $status = 'Green'
+    }
+    elseif($statusCodeGroup -eq '4') {
+        $status = 'Red'
+    }
+    elseif($statusCodeGroup -eq '5') {
+        $status = 'Red'
+    }
+    
+
+    # Output
+    Write-Host "Url: " $url
+    Write-Host "Status: " $statusCode "`n`n" -ForegroundColor $status
 
 }
+
+
+Write-Host "`n`n"
+Write-Host "Warmup completed"
+Write-Host "Touched" $urls.count "Routes"
 
 
 Read-Host
