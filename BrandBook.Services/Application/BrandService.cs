b000b05368269b1eead4a5bdd14f6bec0aebf1cf
Philipp Moser <info@philipp-moser.de>
2020-07-10 23:49:44 +0200

    Setup Structure for BrandService

--- BrandBook.Services/Application/BrandService.cs
+++ BrandBook.Services/Application/BrandService.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BrandBook.Core;
 using BrandBook.Core.Services.Application;
+using BrandBook.Infrastructure;
 
 namespace BrandBook.Services.Application
 {
     public class BrandService : IBrandService
     {
+        #region Fields
+        private readonly IUnitOfWork _unitOfWork;
+        #endregion
+
+
+        #region Constructor
+        public BrandService()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+        #endregion
+
+
+        #region Public Methods
+        #endregion
+
+
+        #region Private Methods
+        #endregion
     }
 }
