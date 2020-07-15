ebdf7f239bd491cd7969f1fc476cc6b6a127273e
Philipp Moser <info@philipp-moser.de>
2020-07-15 22:22:17 +0200

    BB-37: Add CustomHelper For ImagePaths

--- BrandBook.Web.Framework/Helpers/CustomHelper.cs
+++ BrandBook.Web.Framework/Helpers/CustomHelper.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Configuration;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BrandBook.Web.Framework.Helpers
 {
     public static class CustomHelper
     {
+        #region Fields
+        private static readonly string cdnServerUrl = ConfigurationManager.AppSettings["CdnServerUrl"];
+        private static readonly string cdnServerKey = ConfigurationManager.AppSettings["CdnServerKey"];
+        #endregion
+
+        #region Static Methods
+        public static string GetCdnBaseUrl()
+        {
+            return $"https://{cdnServerUrl}/{cdnServerKey}";
+        }
+
+        public static string GetImagePath(string imageName, string area = "")
+        {
+            var imagePath = new StringBuilder();
+
+            imagePath.Append(GetCdnBaseUrl());
+
+            if (string.IsNullOrEmpty(area))
+            {
+                imagePath.Append($"/{area}");
+            }
+
+            imagePath.Append($"/{imageName}");
+
+
+            return imagePath.ToString();
+        }
+        #endregion
     }
 }
