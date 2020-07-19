30665f7b97304521b6a9badb507811e6bb74c67d
Philipp Moser <info@philipp-moser.de>
2020-07-19 21:59:53 +0200

    BB-246: Update FilePath Creation

--- scripts/Deployment/createVersionInfoFile.ps1
+++ scripts/Deployment/createVersionInfoFile.ps1
@@ -1,8 +1,7 @@
-$artifactAlias = "BrandCi.BrandBookV2"
-$versionInfoFileName = "$(System.DefaultWorkingDirectory)/" + $artifactAlias + "/drop/versioninfo.txt"
+$versionInfoFileName = "$(System.DefaultWorkingDirectory)/BrandCi.BrandBookV2/drop/versioninfo.txt"
 
 
 Write-Host "Create Version-Info File"
 
 New-Item $versionInfoFileName
-Set-Content $versionInfoFileName "$(Release.Artifacts." + $artifactAlias + ".SourceVersion)"
+Set-Content $versionInfoFileName "$(Release.Artifacts.BrandCi.BrandBookV2.SourceVersion)"
