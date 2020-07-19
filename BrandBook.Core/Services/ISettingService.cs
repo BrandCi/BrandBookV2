37aba567bc9aa427fff2a96f69b2068168651d96
Philipp Moser <info@philipp-moser.de>
2020-07-19 11:29:20 +0200

    BB-37: Add Methods to ISettingService

--- BrandBook.Core/Services/ISettingService.cs
+++ BrandBook.Core/Services/ISettingService.cs
@@ -2,5 +2,18 @@
 {
     public interface ISettingService
     {
+        /// <summary>
+        /// Get Setting Value from Database-Settings Pool
+        /// </summary>
+        /// <param name="settingKey"></param>
+        /// <returns></returns>
+        string GetSettingValueByKey(string settingKey);
+
+        /// <summary>
+        /// Update Setting Value in Database-Settings Pool
+        /// </summary>
+        /// <param name="settingKey"></param>
+        /// <param name="newValue"></param>
+        void UpdateSettingValue(string settingKey, string newValue);
     }
 }
--- BrandBook.Services/SettingService.cs
+++ BrandBook.Services/SettingService.cs
@@ -4,5 +4,14 @@ namespace BrandBook.Services
 {
     public class SettingService : ISettingService
     {
+        public string GetSettingValueByKey(string settingKey)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateSettingValue(string settingKey, string newValue)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
