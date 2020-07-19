e2fb5a49adbbe69105a648e15f372b91e55662d4
Philipp Moser <info@philipp-moser.de>
2020-07-19 22:37:04 +0200

    BB-246: Update FilePath Working Directory

--- scripts/Deployment/createVersionInfoFile.ps1
+++ scripts/Deployment/createVersionInfoFile.ps1
@@ -1,7 +1,4 @@
-$versionInfoFileName = "$(System.DefaultWorkingDirectory)/BrandCi.BrandBookV2/drop/versioninfo.txt"
-
-
 Write-Host "Create Version-Info File"
 
-New-Item $versionInfoFileName
-Set-Content $versionInfoFileName "$(Release.Artifacts.BrandCi.BrandBookV2.SourceVersion)"
+New-Item "versioninfo.txt"
+Set-Content "versioninfo.txt" "$(Release.Artifacts.BrandCi.BrandBookV2.SourceVersion)"
