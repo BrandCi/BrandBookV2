4e0c32eb210a967e355b6678d157513feca3d823
Philipp Moser <info@philipp-moser.de>
2020-07-21 22:17:23 +0200

    BB-246: Variable adjustment

--- scripts/Deployment/createVersionInfoFile.ps1
+++ scripts/Deployment/createVersionInfoFile.ps1
@@ -1,4 +1,4 @@
 Write-Host "Create Version-Info File"
 
 New-Item "versioninfo.txt"
-Set-Content "versioninfo.txt" $(Release.Artifacts.BrandCi.BrandBookV2.SourceVersion)
+Set-Content "versioninfo.txt" $Release.Artifacts.BrandCi.BrandBookV2.SourceVersion
