445b63c11759dd019bbadc84bed1518da6f2d95f
Philipp Moser <info@philipp-moser.de>
2020-10-04 11:14:46 +0200

    BB-268: Create Notification DomainModel

---
+++ BrandBook.Core/Domain/System/Notification/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BrandBook.Core.Domain.System.Notification
+{
+    public class Notification : BaseEntity
+    {
+        public NotificationTemplateType NotificationTemplateType { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string EmailAddress { get; set; }
+        public string UserName { get; set;}
+        public string RequestId { get; set; }
+        public string EmailContent { get; set; }
+    }
+}
