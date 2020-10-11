489b06e81029991368a2ff842411c132c8c68e57
Philipp Moser <info@philipp-moser.de>
2020-10-11 19:10:37 +0200

    BB-107: Add missing JS files

---
+++ BrandBook.Web/Content/Scripts/Packages/App/UserManagement/app-um-users-overview.js
@@ -0,0 +1,8 @@
+﻿$('#appUserOverview-datatable').DataTable({
+    select: {
+        style: 'multi'
+    },
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/German.json'
+    }
+});
