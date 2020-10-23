9131aac6705e3b5b5cf7fd736424c1c793c562ba
Philipp Moser <info@philipp-moser.de>
2020-10-23 23:07:12 +0200

    BB-274: Setup new Application Theme in Angular Application

--- BrandBook.Web.Application/src/app/app.module.ts
+++ BrandBook.Web.Application/src/app/app.module.ts
@@ -1,37 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from  '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-
-import { SubscriptionComponent } from './components/profile/subscription/subscription.component';
-
+import { AppComponent } from './app.component';
+import { LeftSidebarComponent } from './components/layout/left-sidebar/left-sidebar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubscriptionComponent
+    LeftSidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule,
-    MatCardModule
+    AppRoutingModule
   ],
-  providers: [{provide: APP_BASE_HREF, useValue : '/App/v2' }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
