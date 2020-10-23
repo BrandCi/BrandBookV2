686fc1032c4f345604587e378036dca76adc4c98
Philipp Moser <info@philipp-moser.de>
2020-10-24 00:24:37 +0200

    BB-274: Update Default Route for App/v2

--- BrandBook.Web.Application/src/app/app-routing.module.ts
+++ BrandBook.Web.Application/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'User/CompanyOverview', component: HomeComponent, pathMatch: 'full' },
+    { path: 'v2', component: HomeComponent, pathMatch: 'full' },
     { path: 'Dashboard/RedesignEntry', component: HomeComponent, pathMatch: 'full' }
   ];
 
