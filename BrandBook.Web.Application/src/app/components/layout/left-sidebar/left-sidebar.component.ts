822a7755743c094786199a9fe4ae719073b4e8cf
Philipp Moser <info@philipp-moser.de>
2020-10-25 00:36:20 +0200

    BB-274: Move Navigation Logic to Component

--- BrandBook.Web.Application/src/app/components/layout/left-sidebar/left-sidebar.component.ts
+++ BrandBook.Web.Application/src/app/components/layout/left-sidebar/left-sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
+
 @Component({
   selector: 'app-left-sidebar',
   templateUrl: './left-sidebar.component.html',
@@ -10,6 +13,95 @@ export class LeftSidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+        // handling two columns menu if present
+        var twoColSideNav = $("#two-col-sidenav-main");
+        console.log('twoColSideNav.length: ' + twoColSideNav.length);
+        if (twoColSideNav.length) {
+            console.log('after twoColSiedNav.length');
+            var twoColSideNavItems = $("#two-col-sidenav-main .nav-link");
+            var sideSubMenus = $(".twocolumn-menu-item");
+
+            var nav = $('.twocolumn-menu-item .nav-second-level');
+            var navCollapse = $('#two-col-menu li .collapse');
+
+            // open one menu at a time only
+            navCollapse.on({
+                'show.bs.collapse': function () {
+                    // var nearestNav = $(this).closest(nav).closest(nav).find(navCollapse);
+                    // if (nearestNav.length)
+                        // nearestNav.not($(this)).collapse('hide');
+                    // else
+                        // navCollapse.not($(this)).collapse('hide');
+                }
+            });
+
+            twoColSideNavItems.on('click', function (e) {
+                var target = $($(this).attr('href'));
+
+                console.log('inside changing items');
+
+                if (target.length) {
+                    e.preventDefault();
+
+                    twoColSideNavItems.removeClass('active');
+                    $(this).addClass('active');
+
+                    sideSubMenus.removeClass("d-block");
+                    target.addClass("d-block");
+
+                    // showing full sidebar if menu item is clicked
+                    // $.LayoutThemeApp.leftSidebar.changeSize('default');
+                    return false;
+                }
+                return true;
+            });
+
+            // activate menu with no child
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            twoColSideNavItems.each(function () {
+                if (this.href == pageUrl) {
+                    $(this).addClass('active');
+                }
+            });
+
+
+            // activate the menu in left side bar (Two column) based on url
+            $("#two-col-menu a").each(function () {
+                if (this.href == pageUrl) {
+                    $(this).addClass("active");
+                    $(this).parent().addClass("menuitem-active");
+                    $(this).parent().parent().parent().addClass("show");
+                    $(this).parent().parent().parent().parent().addClass("menuitem-active"); // add active to li of the current link
+
+                    var firstLevelParent = $(this).parent().parent().parent().parent().parent().parent();
+                    if (firstLevelParent.attr('id') !== 'sidebar-menu')
+                        firstLevelParent.addClass("show");
+
+                    $(this).parent().parent().parent().parent().parent().parent().parent().addClass("menuitem-active");
+
+                    var secondLevelParent = $(this).parent().parent().parent().parent().parent().parent().parent().parent().parent();
+                    if (secondLevelParent.attr('id') !== 'wrapper')
+                        secondLevelParent.addClass("show");
+
+                    var upperLevelParent = $(this).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent();
+                    if (!upperLevelParent.is('body'))
+                        upperLevelParent.addClass("menuitem-active");
+
+                    // opening menu
+                    var matchingItem = null;
+                    var targetEl = '#' + $(this).parents('.twocolumn-menu-item').attr("id");
+                    $("#two-col-sidenav-main .nav-link").each(function () {
+                        if ($(this).attr('href') === targetEl) {
+                            matchingItem = $(this);
+                        }
+                    });
+                    if (matchingItem) matchingItem.trigger('click');
+                }
+            });
+        }
+
+
   }
 
 }
