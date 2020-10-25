c9acf03b251d1a878e4bff9e775075698609b401
Philipp Moser <info@philipp-moser.de>
2020-10-25 11:13:28 +0100

    BB-274: Update Routing

--- BrandBook.Web.Application/src/app/app-routing.module.ts
+++ BrandBook.Web.Application/src/app/app-routing.module.ts
@@ -7,7 +7,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'v2', component: DashboardComponent },
-    { path: 'Dashboard', component: DashboardComponent }
+    { path: 'Dashboard', component: DashboardComponent },
+
+    { path: 'User/Profile', component: DashboardComponent },
+    { path: 'User/Settings', component: DashboardComponent },
+    { path: 'User/Subscriptions', component: DashboardComponent },
+
   ];
 
 @NgModule({
